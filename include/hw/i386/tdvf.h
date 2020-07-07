5aad9329d26e0b3173eb678c246d0ef364810c5a
Isaku Yamahata <isaku.yamahata@intel.com>
2020-07-06 22:01:15 -0700

    i386/tdvf: Introduce function to parse TDVF metadata
    TDX VM needs to boot with its specialized firmware, Trusted Domain
    Virtual Firmware (TDVF). QEMU needs to parse TDVF and map it in TD
    guest memory prior to running the TDX VM.
    
    A TDVF Metadata in TDVF image describes the structure of firmware.
    QEMU refers to it to setup memory for TDVF. Introduce function
    tdvf_parse_metadata() to parse the metadata from TDVF image and store
    the info of each TDVF section.
    
    TDX metadata is located by a TDX metadata offset block, which is a
    GUID-ed structure. The data portion of the GUID structure contains
    only an 4-byte field that is the offset of TDX metadata to the end
    of firmware file.
    
    Select X86_FW_OVMF when TDX is enable to leverage existing functions
    to parse and search OVMF's GUID-ed structures.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Co-developed-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Acked-by: Gerd Hoffmann <kraxel@redhat.com>
    
    ---
    Changes in v1:
     - rename tdvf_parse_section_entry() to
       tdvf_parse_and_check_section_entry()
    Changes in RFC v4:
     - rename TDX_METADATA_GUID to TDX_METADATA_OFFSET_GUID

---
+++ include/hw/i386/tdvf.h
@@ -0,0 +1,51 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0-or-later
+
+ * Copyright (c) 2020 Intel Corporation
+ * Author: Isaku Yamahata <isaku.yamahata at gmail.com>
+ *                        <isaku.yamahata at intel.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef HW_I386_TDVF_H
+#define HW_I386_TDVF_H
+
+#include "qemu/osdep.h"
+
+#define TDVF_SECTION_TYPE_BFV               0
+#define TDVF_SECTION_TYPE_CFV               1
+#define TDVF_SECTION_TYPE_TD_HOB            2
+#define TDVF_SECTION_TYPE_TEMP_MEM          3
+
+#define TDVF_SECTION_ATTRIBUTES_MR_EXTEND   (1U << 0)
+#define TDVF_SECTION_ATTRIBUTES_PAGE_AUG    (1U << 1)
+
+typedef struct TdxFirmwareEntry {
+    uint32_t data_offset;
+    uint32_t data_len;
+    uint64_t address;
+    uint64_t size;
+    uint32_t type;
+    uint32_t attributes;
+} TdxFirmwareEntry;
+
+typedef struct TdxFirmware {
+    uint32_t nr_entries;
+    TdxFirmwareEntry *entries;
+} TdxFirmware;
+
+int tdvf_parse_metadata(TdxFirmware *fw, void *flash_ptr, int size);
+
+#endif /* HW_I386_TDVF_H */
