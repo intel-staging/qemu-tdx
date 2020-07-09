a1b4a0583b3198f9a848c0c086e93b6de147466e
Sean Christopherson <sean.j.christopherson@intel.com>
2020-07-09 12:00:20 -0700

    i386/tdx: Add hook to require generic device loader
    Add a hook for TDX to denote that the TD Virtual Firmware must be
    provided via the "generic" device loader.  Error out if pflash is used
    in conjuction with TDX.
    
    Suggested-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>

--- hw/i386/pc_sysfw.c
+++ hw/i386/pc_sysfw.c
@@ -37,6 +37,7 @@
 #include "hw/qdev-properties.h"
 #include "hw/block/flash.h"
 #include "sysemu/kvm.h"
+#include "sysemu/tdx.h"
 #include "sev.h"
 
 #define FLASH_SECTOR_SIZE 4096
@@ -220,6 +221,11 @@ void pc_system_firmware_init(PCMachineState *pcms,
     PCMachineClass *pcmc = PC_MACHINE_GET_CLASS(pcms);
     int i;
     BlockBackend *pflash_blk[ARRAY_SIZE(pcms->flash)];
+
+    if (!tdx_system_firmware_init(pcms, rom_memory)) {
+        pc_system_flash_cleanup_unused(pcms);
+        return;
+    }
 
     if (!pcmc->pci_enabled) {
         x86_bios_rom_init(MACHINE(pcms), "bios.bin", rom_memory, true);
