8d97800b1550a2d3604b58f14ebcf6979d2eb76b
Sean Christopherson <sean.j.christopherson@intel.com>
2020-08-11 15:50:25 -0700

    i386/tdx: Force x2apic mode and routing for TDs
    TDX requires x2apic and "resets" vCPUs to have x2apic enabled.  Model
    this in QEMU and unconditionally enable x2apic interrupt routing.
    
    This fixes issues where interrupts from IRQFD would not get forwarded to
    the guest due to KVM silently dropping the invalid routing entry.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>

--- include/hw/i386/apic.h
+++ include/hw/i386/apic.h
@@ -19,6 +19,7 @@ void apic_init_reset(DeviceState *s);
 void apic_sipi(DeviceState *s);
 void apic_poll_irq(DeviceState *d);
 void apic_designate_bsp(DeviceState *d, bool bsp);
+void apic_force_x2apic(DeviceState *d);
 int apic_get_highest_priority_irr(DeviceState *dev);
 
 /* pc.c */
