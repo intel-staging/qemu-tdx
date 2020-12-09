3e3a755395640c7b4ba4c81dcbe08b5761d68ce5
Isaku Yamahata <isaku.yamahata@intel.com>
2020-12-09 12:24:35 -0800

    target/i386: kvm: don't synchronize guest tsc for TD guest
    Make kvm_synchronize_all_tsc() nop for TD-guest.
    
    TDX module specification, 9.11.1 TSC Virtualization
    "Virtual TSC values are consistent among all the TD;s VCPUs at the
    level suppored by the CPU".
    There is no need for qemu to synchronize tsc and VMM can't access
    to guest TSC. Actually do_kvm_synchronize_tsc() hits assert due to
    failure to write to guest tsc.
    
    > qemu/target/i386/kvm.c:235: kvm_get_tsc: Assertion `ret == 1' failed.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- target/i386/kvm/kvm.c
+++ target/i386/kvm/kvm.c
@@ -258,7 +258,7 @@ void kvm_synchronize_all_tsc(void)
 {
     CPUState *cpu;
 
-    if (kvm_enabled()) {
+    if (kvm_enabled() && vm_type != KVM_X86_TDX_VM) {
         CPU_FOREACH(cpu) {
             run_on_cpu(cpu, do_kvm_synchronize_tsc, RUN_ON_CPU_NULL);
         }
