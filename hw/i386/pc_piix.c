d40e155fb496406898169616e1dc0a8e2c8a2238
Isaku Yamahata <isaku.yamahata@intel.com>
2021-01-12 14:51:46 -0800

    hw/i386: add a flag to disable init/sipi delivery mode of interrupt
    Add a new flag to X86Machine to disallow INIT/SIPI delivery mode of
    interrupt and pass it to ioapic creation so that ioapic disallows
    INIT/SIPI delivery mode.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- hw/i386/pc_piix.c
+++ hw/i386/pc_piix.c
@@ -224,7 +224,7 @@ static void pc_init1(MachineState *machine,
 
     if (pcmc->pci_enabled) {
         ioapic_init_gsi(gsi_state, "i440fx", x86ms->eoi_intercept_unsupported,
-                        x86ms->smi_unsupported);
+                        x86ms->smi_unsupported, x86ms->init_sipi_unsupported);
     }
 
     if (tcg_enabled()) {
