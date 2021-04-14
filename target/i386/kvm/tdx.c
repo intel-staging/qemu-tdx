e6a7f0e07078de0a2766e4e9bda16f65abfaeccc
Isaku Yamahata <isaku.yamahata@intel.com>
2021-04-14 12:17:51 -0700

    i386/tdx: Allows mrconfigid/mrowner/mrownerconfig for TDX_INIT_VM
    When creating TDX vm, three sha384 hash values can be provided for
    TDX attestation.
    
    So far they were hard coded as 0. Now allow user to specify those values
    via property mrconfigid, mrowner and mrownerconfig. String for those
    properties are hex string of 48 * 2 length.
    
    example
    -object tdx-guest, \
      mrconfigid=0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef, \
      mrowner=fedcba9876543210fedcba9876543210fedcba9876543210fedcba9876543210fedcba9876543210fedcba9876543210, \
      mrownerconfig=0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    ---
    TODO:
     - community requests to use base64 encoding if no special reason

--- target/i386/kvm/tdx.c
+++ target/i386/kvm/tdx.c
@@ -802,6 +802,13 @@ int tdx_pre_create_vcpu(CPUState *cpu)
 
     init_vm.init_vm.attributes = tdx_guest->attributes;
 
+    QEMU_BUILD_BUG_ON(sizeof(init_vm.init_vm.mrconfigid) != sizeof(tdx_guest->mrconfigid));
+    QEMU_BUILD_BUG_ON(sizeof(init_vm.init_vm.mrowner) != sizeof(tdx_guest->mrowner));
+    QEMU_BUILD_BUG_ON(sizeof(init_vm.init_vm.mrownerconfig) != sizeof(tdx_guest->mrownerconfig));
+    memcpy(init_vm.init_vm.mrconfigid, tdx_guest->mrconfigid, sizeof(tdx_guest->mrconfigid));
+    memcpy(init_vm.init_vm.mrowner, tdx_guest->mrowner, sizeof(tdx_guest->mrowner));
+    memcpy(init_vm.init_vm.mrownerconfig, tdx_guest->mrownerconfig, sizeof(tdx_guest->mrownerconfig));
+
     do {
         r = tdx_vm_ioctl(KVM_TDX_INIT_VM, 0, &init_vm);
     } while (r == -EAGAIN);
@@ -923,6 +930,12 @@ static void tdx_guest_init(Object *obj)
     object_property_add_bool(obj, "debug",
                              tdx_guest_get_debug,
                              tdx_guest_set_debug);
+    object_property_add_sha384(obj, "mrconfigid", tdx->mrconfigid,
+                               OBJ_PROP_FLAG_READWRITE);
+    object_property_add_sha384(obj, "mrowner", tdx->mrowner,
+                               OBJ_PROP_FLAG_READWRITE);
+    object_property_add_sha384(obj, "mrownerconfig", tdx->mrownerconfig,
+                               OBJ_PROP_FLAG_READWRITE);
 }
 
 static void tdx_guest_finalize(Object *obj)
