intel-staging/qemu-tdx

commit 513ecb21fa8578de14efb05863f8c0a622208a40

i386/tdx: Implement user specified tsc frequency
Reuse -cpu,tsc-frequency= to get user wanted tsc frequency and pass it
to KVM_TDX_INIT_VM.

Besides, sanity check the tsc frequency to be in the legal range and
legal granularity (required by SEAM module).

Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>

target/i386/kvm/tdx.c
@@ -36,6 +36,8 @@
 
 #define TDX1_TD_ATTRIBUTE_DEBUG BIT_ULL(0)
 #define TDX1_TD_ATTRIBUTE_PERFMON BIT_ULL(63)
+#define TDX1_MIN_TSC_FREQUENCY_KHZ (1  * 1000 * 1000)
+#define TDX1_MAX_TSC_FREQUENCY_KHZ (10 * 1000 * 1000)
 
 bool kvm_has_tdx(KVMState *s)
 {
@@ -286,6 +288,19 @@ void tdx_pre_create_vcpu(CPUState *cpu)
         exit(1);
     }
 
+    if (env->tsc_khz && (env->tsc_khz < TDX1_MIN_TSC_FREQUENCY_KHZ ||
+                         env->tsc_khz > TDX1_MAX_TSC_FREQUENCY_KHZ)) {
+        error_report("Invalid TSC %ld KHz, must specify cpu_frequecy between [%d, %d] kHz\n",
+                      env->tsc_khz, TDX1_MIN_TSC_FREQUENCY_KHZ,
+                      TDX1_MAX_TSC_FREQUENCY_KHZ);
+        exit(1);
+    }
+
+    if (env->tsc_khz % (25 * 1000)) {
+        error_report("Invalid TSC %ld KHz, it must be multiple of 25MHz\n", env->tsc_khz);
+        exit(1);
+    }
+
     qemu_mutex_lock(&tdx->lock);
     if (tdx->initialized) {
         goto out;
@@ -298,6 +313,7 @@ void tdx_pre_create_vcpu(CPUState *cpu)
     cpuid_data.cpuid.padding = 0;
 
     init_vm.max_vcpus = ms->smp.cpus;
+    init_vm.tsc_khz = env->tsc_khz;
     init_vm.attributes = 0;
     init_vm.attributes |= tdx->debug ? TDX1_TD_ATTRIBUTE_DEBUG : 0;
     init_vm.attributes |= x86cpu->enable_pmu ? TDX1_TD_ATTRIBUTE_PERFMON : 0;

linux-headers/asm-x86/kvm.h
@@ -529,7 +529,7 @@ struct kvm_tdx_capabilities {
 
 struct kvm_tdx_init_vm {
 	__u32 max_vcpus;
-	__u32 reserved;
+	__u32 tsc_khz;
 	__u64 attributes;
 	__u64 cpuid;
 };

target/i386/kvm/kvm.c
@@ -766,6 +766,14 @@ static int kvm_arch_set_tsc_khz(CPUState *cs)
     int r, cur_freq;
     bool set_ioctl = false;
 
+    /*
+     * TD guest's TSC is immutable, it cannot be set/changed via
+     * KVM_SET_TSC_KHZ, but only be initialized via KVM_TDX_INIT_VM
+     */
+    if (vm_type == KVM_X86_TDX_VM) {
+        return 0;
+    }
+
     if (!env->tsc_khz) {
         return 0;
     }

commit 9c40644cf6b3990290ab5c85a9f8842311831f4b

i386/tdx: Update TDX1_MIN_TSC_FREQUENCY_KHZ to 100MHz
TDX arch lowers the mininal TSC Frequency to 100MHz.

Fixes: 513ecb21fa85 ("i386/tdx: Implement user specified tsc frequency")
Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>

target/i386/kvm/tdx.c
@@ -36,7 +36,7 @@
 
 #define TDX1_TD_ATTRIBUTE_DEBUG BIT_ULL(0)
 #define TDX1_TD_ATTRIBUTE_PERFMON BIT_ULL(63)
-#define TDX1_MIN_TSC_FREQUENCY_KHZ (1  * 1000 * 1000)
+#define TDX1_MIN_TSC_FREQUENCY_KHZ (100 * 1000)
 #define TDX1_MAX_TSC_FREQUENCY_KHZ (10 * 1000 * 1000)
 
 bool kvm_has_tdx(KVMState *s)