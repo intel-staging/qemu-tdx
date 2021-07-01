1313f770675684d2b409952ec72b4e2f049a5111
Yuan Yao <yuan.yao@intel.com>
2021-07-01 09:06:59 +0800

    accel/kvm: Introduce new interface KVMState::set_mr_debug_ops and its wrapper
    This interface is designed to setup the MemoryRegion::debug_ops.
    
    Also introduced 2 wrapper functions for installing/calling the
    KVMState::set_mr_debug_ops from different targets easily.
    
    Signed-off-by: Yuan Yao <yuan.yao@intel.com>

--- include/sysemu/kvm.h
+++ include/sysemu/kvm.h
@@ -588,4 +588,10 @@ int kvm_set_memory_attributes_private(hwaddr start, hwaddr size);
 int kvm_set_memory_attributes_shared(hwaddr start, hwaddr size);
 
 int kvm_convert_memory(hwaddr start, hwaddr size, bool to_private);
+
+typedef void (*set_memory_region_debug_ops)(void *handle, MemoryRegion *mr);
+void kvm_setup_set_memory_region_debug_ops(struct KVMState *s,
+                                           set_memory_region_debug_ops new_ops);
+void kvm_set_memory_region_debug_ops(void *handle, MemoryRegion *mr);
+
 #endif
