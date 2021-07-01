748c2cc2a631ce3ff965cdebab5f5d3e9013da5d
Yuan Yao <yuan.yao@intel.com>
2021-07-01 09:07:03 +0800

    Implement MemoryDebugOps interface for encrypted guest
    Also the new address_space_{read,write} debug helper
    interfaces which can be invoked by vendor specific guest
    memory debug assist/hooks to do guest RAM memory accesses
    using the added MemoryRegion callbacks.
    
    Yuan Yao:
        - Fixed fuzz_dma_read_cb() parameter issue for QEMU 5.2.91.
        - Move the caller of encrypted_memory_debug_ops into phymem.c
          as common callbacks for encrypted guests.
        - Adapted address_space_read_debug/address_space_wirte_rom_debug
          with new definition of MemoryRegion::ram_debug_ops;
        - Install the encrypted_memory_debug_ops/phymem.c for INTEL
          TD guest.
        - Refactor the too long subject line
        - Fill buffer to 0x0 if failed to read guest memory
    
    Signed-off-by: Ashish Kalra <ashish.kalra@amd.com>
    Signed-off-by: Yuan Yao <yuan.yao@intel.com>

--- include/exec/memory.h
+++ include/exec/memory.h
@@ -2586,6 +2586,12 @@ MemTxResult address_space_write_rom(AddressSpace *as, hwaddr addr,
                                     MemTxAttrs attrs,
                                     const void *buf, hwaddr len);
 
+MemTxResult address_space_write_rom_debug(AddressSpace *as,
+                                          hwaddr addr,
+                                          MemTxAttrs attrs,
+                                          const void *ptr,
+                                          hwaddr len);
+
 /* address_space_ld*: load from an address space
  * address_space_st*: store to an address space
  *
@@ -2852,6 +2858,8 @@ extern const MemoryDebugOps *physical_memory_debug_ops;
 
 void address_space_set_debug_ops(const MemoryDebugOps *ops);
 
+void set_encrypted_memory_debug_ops(void);
+
 static inline bool memory_access_is_direct(MemoryRegion *mr, bool is_write)
 {
     if (is_write) {
@@ -2906,6 +2914,10 @@ MemTxResult address_space_read(AddressSpace *as, hwaddr addr,
     }
     return result;
 }
+
+MemTxResult address_space_read_debug(AddressSpace *as, hwaddr addr,
+                                     MemTxAttrs attrs, void *buf,
+                                     hwaddr len);
 
 /**
  * address_space_read_cached: read from a cached RAM region
