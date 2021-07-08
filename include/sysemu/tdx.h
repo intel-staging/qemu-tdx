intel-staging/qemu-tdx

commit fe0bec9296473ae8ab815ea7f418893b0868ab00

qmp: fix query-tdx-capabilities and add query-tdx command
This patch fixes the following qtest error.  For QMP query, it should
return value or set Error.  It always set value instead of error if kvm or
tdx isn't supported.  Opportunistically add query-tdx to query per-VM
info.

  Running test qtest-x86_64/qmp-cmd-test
  ERROR:../../../go/src/qemu.org/qemu-next/tests/qtest/qmp-cmd-test.c:76:test_query: assertion failed: (qdict_haskey(resp, "return"))
  ERROR qtest-x86_64/qmp-cmd-test - Bail out! ERROR:../../../go/src/qemu.org/qemu-next/tests/qtest/qmp-cmd-test.c:76:test_query: assertion failed: (qdict_haskey(resp, "return"))

Signed-off-by: yamahata <isaku.yamahata@intel.com>

target/i386/kvm/tdx.c
@@ -23,6 +23,7 @@
 #include "qapi/error.h"
 #include "qom/object_interfaces.h"
 #include "qapi/qapi-types-misc-target.h"
+#include "qapi/qapi-types-misc-target.h"
 #include "standard-headers/asm-x86/kvm_para.h"
 #include "sysemu/sysemu.h"
 #include "sysemu/runstate-action.h"
@@ -44,18 +45,20 @@ bool kvm_has_tdx(KVMState *s)
     return !!(kvm_check_extension(s, KVM_CAP_VM_TYPES) & BIT(KVM_X86_TDX_VM));
 }
 
+TDXInfo *tdx_get_info(void)
+{
+    TDXInfo *info;
+    info = g_new0(TDXInfo, 1);
+    info->enabled = kvm_tdx_enabled();
+    return info;
+}
+
 TDXCapability *tdx_get_capabilities(void)
 {
     TDXCapability *cap;
 
-    if (!kvm_enabled() || !kvm_has_tdx(kvm_state)) {
-        return NULL;
-    }
-
     cap = g_new0(TDXCapability, 1);
-
-    /* TODO: Determine what data actually needs to be reported. */
-    cap->shared_bit_pos = 51;
+    cap->enabled = kvm_enabled() && kvm_has_tdx(kvm_state);
     return cap;
 }
 

target/i386/kvm/tdx-stub.c
@@ -22,6 +22,11 @@ void tdx_post_init_vcpu(CPUState *cpu)
 {
 }
 
+struct TDXInfo *tdx_get_info(void)
+{
+    return NULL;
+}
+
 struct TDXCapability *tdx_get_capabilities(void)
 {
     return NULL;

target/i386/monitor.c
@@ -759,13 +759,24 @@ void qmp_sev_inject_launch_secret(const char *packet_hdr,
     sev_inject_launch_secret(packet_hdr, secret, gpa, errp);
 }
 
+TDXInfo *qmp_query_tdx(Error **errp)
+{
+    TDXInfo *info;
+
+    info = tdx_get_info();
+    if (!info) {
+        error_setg(errp, "TDX is not available.");
+    }
+    return info;
+}
+
 TDXCapability *qmp_query_tdx_capabilities(Error **errp)
 {
     TDXCapability *cap;
 
     cap = tdx_get_capabilities();
     if (!cap) {
-        error_setg(errp, "TDX is not available");
+        error_setg(errp, "TDX is not available.");
     }
     return cap;
 }

commit 3811c78daf5259ecc4079ff9985204c96bebf352

KVM/TDX/qmp: compile fix
There was mismerge to miss the update of tdx.h and misc-target.json.
Include the change to misc-target.json.

Fixes: fe0bec9296 ("qmp: fix query-tdx-capabilities and add query-tdx command")
Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

include/sysemu/tdx.h
@@ -13,6 +13,9 @@ int tdx_system_firmware_init(PCMachineState *pcms, MemoryRegion *rom_memory);
 void tdx_pre_create_vcpu(CPUState *cpu);
 void tdx_post_init_vcpu(CPUState *cpu);
 
+struct TDXInfo;
+struct TDXInfo *tdx_get_info(void);
+
 struct TDXCapability;
 struct TDXCapability *tdx_get_capabilities(void);
 

target/i386/kvm/tdx.c
@@ -48,8 +48,9 @@ bool kvm_has_tdx(KVMState *s)
 TDXInfo *tdx_get_info(void)
 {
     TDXInfo *info;
+
     info = g_new0(TDXInfo, 1);
-    info->enabled = kvm_tdx_enabled();
+    info->enabled = kvm_enabled() && kvm_tdx_enabled();
     return info;
 }
 