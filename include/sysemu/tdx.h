e2f0221e845dfeb0170f8668620af9c5c5f06fc9
Xiaoyao Li <xiaoyao.li@intel.com>
2021-07-30 17:06:28 +0800

    i386/tdx: Update XFAM related features when XFAM settles down
    For XFAM-allowed features, they follow the virtualization type "As
    Configured (if Native)". Thus, they are configurable if native supports.
    TDX-SEAM will masks off all the related CPUID bits if the corresponding
    XFAM bits is configured off. e.g. if XFAM[2] is off, avx, avx2, vaes,
    etc are all disabled in TD guest.
    
    XFAM is consistent with the XSAVE component features, which has the same
    format as XCR0 or IA32_XSS MSR. It can be reported by CPUID(0xD, 0x1).
    For the configurable xsave component features, ideally, all XFAM-allowed
    features are considered to be configurable. However, in TDX, when the
    QEMU tries to disable one of them (e.g. -avx512dq), XFAM[7:5] would be
    cleared. Then all of other avx512 features will be disabled. To
    distinguish such possible unintentional behavior, the user can only disable
    the whole avx512-related features by disable a delegate feature (e.g.
    -avx512f), or be told that it is not allowed to do the modification when
    minus other avx512 features.
    
    In last stage of CPUID expansion, the dependency check for xfam features
    will be carried out. Although some features are not yet supported in
    KVM, they should still be considered.
    
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Co-developed-by: Chenyi Qiang <chenyi.qiang@intel.com>
    Signed-off-by: Chenyi Qiang <chenyi.qiang@intel.com>

--- include/sysemu/tdx.h
+++ include/sysemu/tdx.h
@@ -12,6 +12,7 @@ int tdx_system_firmware_init(PCMachineState *pcms, MemoryRegion *rom_memory);
 
 void tdx_pre_create_vcpu(CPUState *cpu);
 void tdx_post_init_vcpu(CPUState *cpu);
+void tdx_update_xfam_features(CPUState *cpu);
 
 struct TDXInfo;
 struct TDXInfo *tdx_get_info(void);
