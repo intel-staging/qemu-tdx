intel-staging/qemu-tdx

commit 2ff02aa44e34069b245e3f6635c5b897c8c19454

i386/tdx: Extract tdx_get_cpuid_config to calculate the configurable CPUID for TDX
tdx_get_supported_cpuid can return the TDX-supported CPUID bits.
Extract the function tdx_get_cpuid_config out of it to calculate the
host VMM configurable CPUID bits.

According to Chapter 16.2 in [1], Intel TDX module virtualizes CPUID to
guest TDs in five types. They are:
-As configured: reflect VMM configuration
-As configured (if native): if configured, native value returned.
otherwise 0.
-Fixed: fixed to 0/1
-Native: reflect the native value
-Calculated: calculated by TDX module.
(Calculated type is not accurate in QEMU/KVM as it requires TDX module's
manupulation, ignore it in TD creation stage)

In order to keep an aligned CPUID in QEMU/KVM/TD guest, meanwhile make
the way to +feature/-feature check support in QEMU, follow all above types
restriction individually when querying TDX supported CPUID.

1) CPUID bits that belong to "Fixed" type can be reflected in
tdx_fixed0/1 field of tdx_cpuid_lookup defined in QEMU,
xfam_fixed0/xfam_fixed1 and attrs_fixed0/attrs_fixed1 fields in tdx_caps
returned from KVM. Host VMM can't change these values.

2) For "As Configured" type features, they are reflected in
tdx_caps->cpuid_config[] and some special cases (e.g. tsc_frequency in
CPUID leaf 0x15 is handled separately). Host VMM can configure it if
needed.

3) For "As configured (if native)" features, they can be obversed in
tdx_caps->cpuid_configs[] if native supports it. Besides, XFAM-allowed
and ATTRIBUTES-allowed features also belong to this type, and will be
introduced in following patches. Host VMM can configure them on demand.

4) There's also an implict type which are not listed in spec, because
TDX module won't emulate it and just injects #VE exception into TD
guest. If TD guest still requires its emulation, it can tdvmcall into
KVM for further handling. Call this type of CPUID bit as "Fault", such
as CPUID leaf 0x1f and 0x6. They should obey KVM's implementation, so

target/i386/kvm/tdx.c
@@ -79,6 +79,13 @@
             (1ULL << KVM_FEATURE_ASYNC_PF) | \
             (1ULL << KVM_FEATURE_ASYNC_PF_VMEXIT))
 
+/* Some KVM PV features are treated as configurable */
+#define TDX_CONFIG_KVM_FEATURES ((1ULL << KVM_FEATURE_NOP_IO_DELAY) | \
+            (1ULL << KVM_FEATURE_PV_EOI) | (1ULL << KVM_FEATURE_PV_UNHALT) | \
+            (1ULL << KVM_FEATURE_PV_TLB_FLUSH) | (1ULL << KVM_FEATURE_PV_SEND_IPI) | \
+            (1ULL << KVM_FEATURE_POLL_CONTROL) | (1ULL << KVM_FEATURE_PV_SCHED_YIELD) | \
+            (1ULL << KVM_FEATURE_ASYNC_PF_INT) | (1ULL << KVM_FEATURE_MSI_EXT_DEST_ID))
+
 typedef struct kvm_tdx_cpuid_lookup {
   uint64_t tdx_fixed0;
   uint64_t tdx_fixed1;
@@ -301,50 +308,162 @@ int tdx_system_firmware_init(PCMachineState *pcms, MemoryRegion *rom_memory)
     return 0;
 }
 
-void tdx_get_supported_cpuid(KVMState *s, uint32_t function,
-                             uint32_t index, int reg, uint32_t *ret)
+extern FeatureWordInfo feature_word_info[FEATURE_WORDS];
+
+uint32_t tdx_get_cpuid_config(uint32_t function, uint32_t index, int reg)
+{
+    struct kvm_tdx_cpuid_config *cpuid_c;
+    int i;
+    uint32_t ret = 0;
+    uint32_t eax, ebx, ecx, edx;
+    FeatureWord w;
+
+    if (function == KVM_CPUID_FEATURES && reg == R_EAX) {
+        return TDX_CONFIG_KVM_FEATURES;
+    }
+
+    /* Check if native supports */
+    host_cpuid(function, index, &eax, &ebx, &ecx, &edx);
+
+    /* TODO: AMX in XCR0 is not yet configurable */
+    if (function == 0xd && index == 0x0 && reg == R_EAX) {
+        return XCR0_MASK & eax;
+    }
+
+    /*
+     * fault type of CPUID are those will cause #VE injected
+     * into TD guest, which could transfer to KVM to handle it.
+     * Thus, see them as configurable and apply KVM's configuration.
+     */
+    for (w = 0; w < FEATURE_WORDS; w++) {
+        FeatureWordInfo *f = &feature_word_info[w];
+
+        if (f->type == MSR_FEATURE_WORD) {
+            continue;
+        }
+
+        if (f->cpuid.eax == function && f->cpuid.reg == reg &&
+            (!f->cpuid.needs_ecx || f->cpuid.ecx == index) &&
+            tdx_cpuid_lookup[w].faulting) {
+            switch (reg) {
+            case R_EAX:
+                ret |= eax;
+                break;
+            case R_EBX:
+                ret |= ebx;
+                break;
+            case R_ECX:
+                ret |= ecx;
+                break;
+            case R_EDX:
+                ret |= edx;
+                break;
+            default:
+                return 0;
+            }
+
+            return ret;
+        }
+    }
+
+    if (tdx_caps->nr_cpuid_configs <= 0) {
+        return ret;
+    }
+
+    for (i = 0; i < tdx_caps->nr_cpuid_configs; i++) {
+        cpuid_c = &tdx_caps->cpuid_configs[i];
+        /* The sub-leaf of function 0x1 is 0xffffffff in tdx_caps */
+        if (cpuid_c->leaf == function && (cpuid_c->sub_leaf == index ||
+            function == 0x1)) {
+            switch (reg) {
+            case R_EAX:
+                ret |= cpuid_c->eax;
+                break;
+            case R_EBX:
+                ret |= cpuid_c->ebx;
+                break;
+            case R_ECX:
+                ret |= cpuid_c->ecx;
+                break;
+            case R_EDX:
+                ret |= cpuid_c->edx;
+                break;
+            default:
+                return 0;
+            }
+        }
+    }
+
+    return ret;
+}
+
+uint32_t tdx_get_supported_cpuid(uint32_t function, uint32_t index, int reg)
 {
     MachineState *ms = MACHINE(qdev_get_machine());
     TdxGuest *tdx = (TdxGuest *)object_dynamic_cast(OBJECT(ms->cgs),
                                                     TYPE_TDX_GUEST);
+    uint32_t eax, ebx, ecx, edx, tdx_config;
+    uint32_t ret = 0;
+    FeatureWord w;
 
     if (!tdx) {
-        return;
+        return ret;
     }
 
-    switch (function) {
-    case 1:
-        if (reg == R_ECX) {
-            *ret &= ~CPUID_EXT_VMX;
+    for (w = 0; w < FEATURE_WORDS; w++) {
+        FeatureWordInfo *f = &feature_word_info[w];
+
+        if (f->type == MSR_FEATURE_WORD) {
+            continue;
         }
-        break;
-    case 0xd:
-        if (index == 0) {
+
+        if (f->cpuid.eax != function || f->cpuid.reg != reg ||
+            (f->cpuid.needs_ecx && f->cpuid.ecx != index)) {
+            continue;
+        }
+
+        /* Start from native value */
+        host_cpuid(function, index, &eax, &ebx, &ecx, &edx);
+
+        switch (reg) {
+        case R_EAX:
+            ret |= eax;
+            break;
+        case R_EBX:
+            ret |= ebx;
+            break;
+        case R_ECX:
+            ret |= ecx;
+            break;
+        case R_EDX:
+            ret |= edx;
+            break;
+        }
+
+        /* tdx_cap->xfam_fixed check */
+        if (function == 0xd && index == 0) {
             if (reg == R_EAX) {
-                *ret &= (uint32_t)tdx_caps->xfam_fixed0 & XCR0_MASK;
-                *ret |= (uint32_t)tdx_caps->xfam_fixed1 & XCR0_MASK;
+                ret &= (uint32_t)tdx_caps->xfam_fixed0 & XCR0_MASK;
+                ret |= (uint32_t)tdx_caps->xfam_fixed1 & XCR0_MASK;
             } else if (reg == R_EDX) {
-                *ret &= (tdx_caps->xfam_fixed0 & XCR0_MASK) >> 32;
-                *ret |= (tdx_caps->xfam_fixed1 & XCR0_MASK) >> 32;
+                ret &= (tdx_caps->xfam_fixed0 & XCR0_MASK) >> 32;
+                ret |= (tdx_caps->xfam_fixed1 & XCR0_MASK) >> 32;
             }
-        } else if (index == 1) {
-            /* TODO: Adjust XSS when it's supported. */
+            return ret;
         }
-        break;
-    case KVM_CPUID_FEATURES:
-        if (reg == R_EAX) {
-            *ret &= ~((1ULL << KVM_FEATURE_CLOCKSOURCE) |
-                      (1ULL << KVM_FEATURE_CLOCKSOURCE2) |
-                      (1ULL << KVM_FEATURE_CLOCKSOURCE_STABLE_BIT) |
-                      (1ULL << KVM_FEATURE_ASYNC_PF) |
-                      (1ULL << KVM_FEATURE_ASYNC_PF_VMEXIT) |
-                      (1ULL << KVM_FEATURE_ASYNC_PF_INT));
-        }
-        break;
-    default:
-        /* TODO: Use tdx_caps to adjust CPUID leafs. */
-        break;
+
+        /* configurable cpuid are supported by TDX unconditionally */
+        tdx_config = tdx_get_cpuid_config(function, index, reg);
+        ret |= tdx_config;
+
+        /* enforce "fixed" type CPUID virtualization */
+        ret |= tdx_cpuid_lookup[w].tdx_fixed1;
+        ret &= ~tdx_cpuid_lookup[w].tdx_fixed0;
+
+        return ret;
     }
+
+    return ret;
 }
 
 void tdx_pre_create_vcpu(CPUState *cpu)

target/i386/kvm/kvm.c
@@ -375,7 +375,8 @@ uint32_t kvm_arch_get_supported_cpuid(KVMState *s, uint32_t function,
 {
     struct kvm_cpuid2 *cpuid;
     uint32_t ret = 0;
-    uint32_t cpuid_1_edx;
+    uint32_t cpuid_1_edx, cpuid_config;
+    uint32_t tdx_cpuid = 0;
 
     cpuid = get_supported_cpuid(s);
 
@@ -460,7 +461,14 @@ uint32_t kvm_arch_get_supported_cpuid(KVMState *s, uint32_t function,
         ret |= 1U << KVM_HINTS_REALTIME;
     }
 
-    tdx_get_supported_cpuid(s, function, index, reg, &ret);
+    if (kvm_tdx_enabled()) {
+        tdx_cpuid = tdx_get_supported_cpuid(function, index, reg);
+        cpuid_config = tdx_get_cpuid_config(function, index, reg);
+        /* Only apply KVM values when the CPUID bits are configurable, others
+         * should follow TDX requirement.
+         */
+        ret = (ret & cpuid_config) | (tdx_cpuid & ~cpuid_config);
+    }
 
     return ret;
 }

target/i386/kvm/tdx.h
@@ -52,7 +52,7 @@ typedef struct TdxGuest {
 } TdxGuest;
 
 int tdx_kvm_init(ConfidentialGuestSupport *cgs, Error **errp);
-void tdx_get_supported_cpuid(KVMState *s, uint32_t function,
-                             uint32_t index, int reg, uint32_t *ret);
+uint32_t tdx_get_supported_cpuid(uint32_t function, uint32_t index, int reg);
+uint32_t tdx_get_cpuid_config(uint32_t function, uint32_t index, int reg);
 
 #endif

commit 2fb7b9b733fb27efb45b1c7e9bac3d1ed193c554

target/i386: Clear KVM_FEATURE_ASYNC_PF_INT in configurable features
TDX doesn't support async page fault, users can't configure it.

Fixes: 2ff02aa44e("i386/tdx: Extract tdx_get_cpuid_config to calculate
the configurable CPUID for TDX")
Signed-off-by: Chenyi Qiang <chenyi.qiang@intel.com>

target/i386/kvm/tdx.c
@@ -90,7 +90,7 @@
             (1ULL << KVM_FEATURE_PV_EOI) | (1ULL << KVM_FEATURE_PV_UNHALT) | \
             (1ULL << KVM_FEATURE_PV_TLB_FLUSH) | (1ULL << KVM_FEATURE_PV_SEND_IPI) | \
             (1ULL << KVM_FEATURE_POLL_CONTROL) | (1ULL << KVM_FEATURE_PV_SCHED_YIELD) | \
-            (1ULL << KVM_FEATURE_ASYNC_PF_INT) | (1ULL << KVM_FEATURE_MSI_EXT_DEST_ID))
+            (1ULL << KVM_FEATURE_MSI_EXT_DEST_ID))
 
 typedef struct kvm_tdx_cpuid_lookup {
   uint64_t tdx_fixed0;