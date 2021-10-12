intel-staging/qemu-tdx

commit eb87e9bfa9b2c6af682dc7653bef16c25f858ea3

i386/tdx: Update XFAM related features when XFAM settles down
For XFAM-allowed features, they follow the virtualization type "As
Configured (if Native)". Thus, they are configurable if native supports.
TDX-SEAM will masks off all the related CPUID bits if the corresponding
XFAM bits is configured off. e.g. if XFAM[2] is off, avx, avx2, vaes,
etc are all disabled in TD guest.

XFAM is consistent with the XSAVE component features, which has the same
format as XCR0 or IA32_XSS MSR. It can be reported by CPUID(0xD, 0x1).
For the configurable xsave component features, ideally, all XFAM-allowed
features are considered to be configurable. However, in TDX, when the
QEMU tries to disable one of them (e.g. -avx512dq), XFAM[7:5] would be
cleared. Then all of other avx512 features will be disabled. To
distinguish such possible unintentional behavior, the user can only disable
the whole avx512-related features by disable a delegate feature (e.g.
-avx512f), or be told that it is not allowed to do the modification when
minus other avx512 features.

In last stage of CPUID expansion, the dependency check for xfam features
will be carried out. Although some features are not yet supported in
KVM, they should still be considered.

Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>
Co-developed-by: Chenyi Qiang <chenyi.qiang@intel.com>
Signed-off-by: Chenyi Qiang <chenyi.qiang@intel.com>

target/i386/kvm/tdx.c
@@ -17,6 +17,7 @@
 #include <sys/ioctl.h>
 
 #include "cpu.h"
+#include "cpu-internal.h"
 #include "kvm_i386.h"
 #include "hw/boards.h"
 #include "hw/i386/tdvf-hob.h"
@@ -254,7 +255,7 @@ static struct {
 
 static struct kvm_tdx_capabilities *tdx_caps = (void *)&__tdx_caps;
 
-#define XCR0_MASK (MAKE_64BIT_MASK(0, 8) | BIT_ULL(9))
+#define XCR0_MASK (MAKE_64BIT_MASK(0, 8) | BIT_ULL(9) | MAKE_64BIT_MASK(17, 2))
 #define XSS_MASK (~XCR0_MASK)
 
 int tdx_kvm_init(ConfidentialGuestSupport *cgs, Error **errp)
@@ -308,14 +309,84 @@ int tdx_system_firmware_init(PCMachineState *pcms, MemoryRegion *rom_memory)
     return 0;
 }
 
-extern FeatureWordInfo feature_word_info[FEATURE_WORDS];
+static FeatureDep xfam_dependencies[] = {
+    /* XFAM[7:5] may be set to 111 only when XFAM[2] is set to 1 */
+    {
+        .from = { FEAT_XSAVE_COMP_LO, XSTATE_YMM_MASK },
+        .to = { FEAT_XSAVE_COMP_LO, XSTATE_AVX_512_MASK },
+    },
+    {
+        .from = { FEAT_XSAVE_COMP_LO, XSTATE_YMM_MASK },
+        .to = { FEAT_1_ECX,
+                CPUID_EXT_FMA | CPUID_EXT_AVX |
+                CPUID_EXT_F16C },
+    },
+    {
+        .from = { FEAT_XSAVE_COMP_LO, XSTATE_YMM_MASK },
+        .to = { FEAT_7_0_EBX, CPUID_7_0_EBX_AVX2 },
+    },
+    {
+        .from = { FEAT_XSAVE_COMP_LO, XSTATE_YMM_MASK },
+        .to = { FEAT_7_0_ECX, CPUID_7_0_ECX_VAES | CPUID_7_0_ECX_VPCLMULQDQ},
+    },
+    {
+        .from = { FEAT_XSAVE_COMP_LO, XSTATE_AVX_512_MASK },
+        .to = { FEAT_7_0_EBX,
+                CPUID_7_0_EBX_AVX512F | CPUID_7_0_EBX_AVX512DQ |
+                CPUID_7_0_EBX_AVX512IFMA | CPUID_7_0_EBX_AVX512PF |
+                CPUID_7_0_EBX_AVX512ER | CPUID_7_0_EBX_AVX512CD |
+                CPUID_7_0_EBX_AVX512BW | CPUID_7_0_EBX_AVX512VL },
+    },
+    {
+        .from = { FEAT_XSAVE_COMP_LO, XSTATE_AVX_512_MASK },
+        .to = { FEAT_7_0_ECX,
+                CPUID_7_0_ECX_AVX512_VBMI | CPUID_7_0_ECX_AVX512_VBMI2 |
+                CPUID_7_0_ECX_AVX512VNNI | CPUID_7_0_ECX_AVX512BITALG |
+                CPUID_7_0_ECX_AVX512_VPOPCNTDQ },
+    },
+    {
+        .from = { FEAT_XSAVE_COMP_LO, XSTATE_AVX_512_MASK },
+        .to = { FEAT_7_0_EDX,
+                CPUID_7_0_EDX_AVX512_4VNNIW | CPUID_7_0_EDX_AVX512_4FMAPS |
+                CPUID_7_0_EDX_AVX512_VP2INTERSECT | CPUID_7_0_EDX_AVX512_FP16 },
+    },
+    {
+        .from = { FEAT_XSAVE_COMP_LO, XSTATE_AVX_512_MASK },
+        .to = { FEAT_7_1_EAX, CPUID_7_1_EAX_AVX512_BF16 | CPUID_7_1_EAX_AVX_VNNI },
+    },
+    {
+        .from = { FEAT_XSAVE_COMP_LO, XSTATE_PKRU_MASK },
+        .to = { FEAT_7_0_ECX, CPUID_7_0_ECX_PKU },
+    },
+    {
+        .from = { FEAT_XSAVE_COMP_LO, XSTATE_AMX_MASK },
+        .to = { FEAT_7_0_EDX, CPUID_7_0_EDX_AMX_BF16 | CPUID_7_0_EDX_AMX_TILE |
+                CPUID_7_0_EDX_AMX_INT8}
+    },
+    /* TODO: XSS features */
+};
+
+/*
+ * Select a delegate feature for each XFAM-allowed features. e.g avx for all XFAM[2].
+ * Only the delegate one is allowed to be configured. This can help prevent unintentional
+ * mistake by the user.
+ */
+FeatureMask tdx_xfam_feature_delegate[] = {
+    [XSTATE_YMM_BIT] = { .index = FEAT_1_ECX, .mask = CPUID_EXT_AVX },
+    [XSTATE_OPMASK_BIT] = { .index = FEAT_7_0_EBX, .mask = CPUID_7_0_EBX_AVX512F },
+    [XSTATE_ZMM_Hi256_BIT] = { .index = FEAT_7_0_EBX, .mask = CPUID_7_0_EBX_AVX512F },
+    [XSTATE_Hi16_ZMM_BIT] = { .index = FEAT_7_0_EBX, .mask = CPUID_7_0_EBX_AVX512F },
+    [XSTATE_PKRU_BIT] = { .index = FEAT_7_0_ECX, .mask = CPUID_7_0_ECX_PKU },
+    [XSTATE_XTILE_CFG_BIT] = { .index = FEAT_7_0_EDX, .mask = CPUID_7_0_EDX_AMX_BF16 },
+    [XSTATE_XTILE_DATA_BIT] = { .index = FEAT_7_0_EDX, .mask = CPUID_7_0_EDX_AMX_BF16 },
+};
 
 uint32_t tdx_get_cpuid_config(uint32_t function, uint32_t index, int reg)
 {
     struct kvm_tdx_cpuid_config *cpuid_c;
     int i;
     uint32_t ret = 0;
-    uint32_t eax, ebx, ecx, edx;
+    uint32_t eax, ebx, ecx, edx, native, xfam_fixed;
     FeatureWord w;
 
     if (function == KVM_CPUID_FEATURES && reg == R_EAX) {
@@ -325,9 +396,11 @@ uint32_t tdx_get_cpuid_config(uint32_t function, uint32_t index, int reg)
     /* Check if native supports */
     host_cpuid(function, index, &eax, &ebx, &ecx, &edx);
 
-    /* TODO: AMX in XCR0 is not yet configurable */
+    xfam_fixed = (uint32_t)tdx_caps->xfam_fixed1 |
+                ~(uint32_t)tdx_caps->xfam_fixed0;
+
     if (function == 0xd && index == 0x0 && reg == R_EAX) {
-        return XCR0_MASK & eax;
+        return (XCR0_MASK & ~xfam_fixed) & eax;
     }
 
     /*
@@ -343,26 +416,34 @@ uint32_t tdx_get_cpuid_config(uint32_t function, uint32_t index, int reg)
         }
 
         if (f->cpuid.eax == function && f->cpuid.reg == reg &&
-            (!f->cpuid.needs_ecx || f->cpuid.ecx == index) &&
-            tdx_cpuid_lookup[w].faulting) {
+            (!f->cpuid.needs_ecx || f->cpuid.ecx == index)) {
             switch (reg) {
             case R_EAX:
-                ret |= eax;
+                native = eax;
                 break;
             case R_EBX:
-                ret |= ebx;
+                native = ebx;
                 break;
             case R_ECX:
-                ret |= ecx;
+                native = ecx;
                 break;
             case R_EDX:
-                ret |= edx;
+                native = edx;
                 break;
             default:
                 return 0;
             }
-
-            return ret;
+            if (tdx_cpuid_lookup[w].faulting) {
+                return native;
+            }
+            /* Add XFAM-allowed configurable features */
+            for (i = 0; i < ARRAY_SIZE(tdx_xfam_feature_delegate); i++) {
+                FeatureMask *d = &tdx_xfam_feature_delegate[i];
+                if (d->index == w && !(xfam_fixed & (1ULL << i))) {
+                    ret |= (d->mask & native);
+                }
+            }
+            break;
         }
     }
 
@@ -464,6 +545,34 @@ uint32_t tdx_get_supported_cpuid(uint32_t function, uint32_t index, int reg)
     }
 
     return ret;
+}
+
+void tdx_update_xfam_features(CPUState *cpu)
+{
+    MachineState *ms = MACHINE(qdev_get_machine());
+    X86CPU *x86_cpu = X86_CPU(cpu);
+    CPUX86State *env = &x86_cpu->env;
+    int i;
+    TdxGuest *tdx = (TdxGuest *)object_dynamic_cast(OBJECT(ms->cgs),
+                                                    TYPE_TDX_GUEST);
+
+    if (!tdx) {
+        return;
+    }
+
+    for (i = 0; i < ARRAY_SIZE(xfam_dependencies); i++) {
+        FeatureDep *d = &xfam_dependencies[i];
+        if (!(env->features[d->from.index] & d->from.mask)) {
+            uint64_t unavailable_features = env->features[d->to.index] & d->to.mask;
+
+            /* Not an error unless the dependent feature was added explicitly */
+            mark_unsuitable_features(x86_cpu, d->to.index,
+                                     unavailable_features & env->user_plus_features[d->to.index],
+                                     "This feature depends on unrequested XFAM feature",
+                                     true);
+            env->features[d->to.index] &= ~unavailable_features;
+        }
+    }
 }
 
 void tdx_pre_create_vcpu(CPUState *cpu)

include/sysemu/tdx.h
@@ -12,6 +12,7 @@ int tdx_system_firmware_init(PCMachineState *pcms, MemoryRegion *rom_memory);
 
 void tdx_pre_create_vcpu(CPUState *cpu);
 void tdx_post_init_vcpu(CPUState *cpu);
+void tdx_update_xfam_features(CPUState *cpu);
 
 struct TDXInfo;
 struct TDXInfo *tdx_get_info(void);

target/i386/cpu-internal.h
@@ -20,6 +20,15 @@
 #ifndef I386_CPU_INTERNAL_H
 #define I386_CPU_INTERNAL_H
 
+typedef struct FeatureMask {
+    FeatureWord index;
+    uint64_t mask;
+} FeatureMask;
+
+typedef struct FeatureDep {
+    FeatureMask from, to;
+} FeatureDep;
+
 typedef enum FeatureWordType {
    CPUID_FEATURE_WORD,
    MSR_FEATURE_WORD,

target/i386/cpu.c
@@ -25,6 +25,7 @@
 #include "tcg/helper-tcg.h"
 #include "sysemu/reset.h"
 #include "sysemu/hvf.h"
+#include "sysemu/tdx.h"
 #include "kvm/kvm_i386.h"
 #include "sev_i386.h"
 #include "qapi/qapi-visit-machine.h"
@@ -1182,15 +1183,6 @@ FeatureWordInfo feature_word_info[FEATURE_WORDS] = {
      },
 
 };
-
-typedef struct FeatureMask {
-    FeatureWord index;
-    uint64_t mask;
-} FeatureMask;
-
-typedef struct FeatureDep {
-    FeatureMask from, to;
-} FeatureDep;
 
 static FeatureDep feature_dependencies[] = {
     {
@@ -6015,6 +6007,9 @@ void x86_cpu_expand_features(X86CPU *cpu, Error **errp)
 
     if (kvm_enabled()) {
         kvm_hyperv_expand_features(cpu, errp);
+    }
+    if (kvm_tdx_enabled()) {
+        tdx_update_xfam_features(CPU(cpu));
     }
 }
 

target/i386/cpu.h
@@ -523,6 +523,8 @@ typedef enum X86Seg {
 #define XSTATE_ZMM_Hi256_BIT            6
 #define XSTATE_Hi16_ZMM_BIT             7
 #define XSTATE_PKRU_BIT                 9
+#define XSTATE_XTILE_CFG_BIT            17
+#define XSTATE_XTILE_DATA_BIT           18
 
 #define XSTATE_FP_MASK                  (1ULL << XSTATE_FP_BIT)
 #define XSTATE_SSE_MASK                 (1ULL << XSTATE_SSE_BIT)
@@ -533,6 +535,14 @@ typedef enum X86Seg {
 #define XSTATE_ZMM_Hi256_MASK           (1ULL << XSTATE_ZMM_Hi256_BIT)
 #define XSTATE_Hi16_ZMM_MASK            (1ULL << XSTATE_Hi16_ZMM_BIT)
 #define XSTATE_PKRU_MASK                (1ULL << XSTATE_PKRU_BIT)
+#define XSTATE_XTILE_CFG_MASK           (1ULL << XSTATE_XTILE_CFG_BIT)
+#define XSTATE_XTILE_DATA_MASK          (1ULL << XSTATE_XTILE_DATA_BIT)
+
+#define XSTATE_AVX_512_MASK             (XSTATE_OPMASK_MASK |       \
+                                         XSTATE_ZMM_Hi256_MASK |    \
+                                         XSTATE_Hi16_ZMM_MASK)
+#define XSTATE_AMX_MASK                 (XSTATE_XTILE_CFG_MASK |    \
+                                         XSTATE_XTILE_DATA_MASK)
 
 /* CPUID feature words */
 typedef enum FeatureWord {
@@ -829,8 +839,14 @@ typedef uint64_t FeatureWordArray[FEATURE_WORDS];
 #define CPUID_7_0_EDX_SERIALIZE         (1U << 14)
 /* TSX Suspend Load Address Tracking instruction */
 #define CPUID_7_0_EDX_TSX_LDTRK         (1U << 16)
+/* Intel AMX BF16 Support */
+#define CPUID_7_0_EDX_AMX_BF16          (1U << 22)
 /* AVX512_FP16 instruction */
 #define CPUID_7_0_EDX_AVX512_FP16       (1U << 23)
+/* Intel AMX TILE (two dimensional registers) Support */
+#define CPUID_7_0_EDX_AMX_TILE          (1U << 24)
+/* Intel AMX INT8 Support */
+#define CPUID_7_0_EDX_AMX_INT8          (1U << 25)
 /* Speculation Control */
 #define CPUID_7_0_EDX_SPEC_CTRL         (1U << 26)
 /* Single Thread Indirect Branch Predictors */

target/i386/kvm/tdx-stub.c
@@ -27,6 +27,10 @@ struct TDXInfo *tdx_get_info(void)
     return NULL;
 }
 
+void tdx_update_xfam_features(CPUState *cpu)
+{
+}
+
 struct TDXCapability *tdx_get_capabilities(void)
 {
     return NULL;

commit ca166d23947bae60f698777e143f789ea5c8193c

i386/tdx: Fix TDX configurable CPUID
There are some CPUIDs not included in env->features[], e.g.
CPUID[0xA], CPUID[0x14]. These features are either native virtualization
type or depending on other CPUID or props (enable_pmu and INTEL_PT).
They receive the value from KVM directly and will be set in
cpu_x86_cpuid. It is safe not to add more restrictions on them and use
the KVM's value directly, because they don't contian the important
control bits during TD INIT and also refer to their dependent CPUID or
props.

Fixes: eb87e9bfa9("i386/tdx: Update XFAM related features when XFAM settles down")
Signed-off-by: Chenyi Qiang <chenyi.qiang@intel.com>

target/i386/kvm/tdx.c
@@ -439,6 +439,23 @@ uint32_t tdx_get_cpuid_config(uint32_t function, uint32_t index, int reg)
     /* Check if native supports */
     host_cpuid(function, index, &eax, &ebx, &ecx, &edx);
 
+    switch (reg) {
+    case R_EAX:
+        native = eax;
+        break;
+    case R_EBX:
+        native = ebx;
+        break;
+    case R_ECX:
+        native = ecx;
+        break;
+    case R_EDX:
+        native = edx;
+        break;
+    default:
+        return 0;
+    }
+
     xfam_fixed = (uint32_t)tdx_caps->xfam_fixed1 |
                 ~(uint32_t)tdx_caps->xfam_fixed0;
 
@@ -467,22 +484,7 @@ uint32_t tdx_get_cpuid_config(uint32_t function, uint32_t index, int reg)
 
         if (f->cpuid.eax == function && f->cpuid.reg == reg &&
             (!f->cpuid.needs_ecx || f->cpuid.ecx == index)) {
-            switch (reg) {
-            case R_EAX:
-                native = eax;
-                break;
-            case R_EBX:
-                native = ebx;
-                break;
-            case R_ECX:
-                native = ecx;
-                break;
-            case R_EDX:
-                native = edx;
-                break;
-            default:
-                return 0;
-            }
+
             if (tdx_cpuid_lookup[w].faulting) {
                 return native;
             }
@@ -530,6 +532,15 @@ uint32_t tdx_get_cpuid_config(uint32_t function, uint32_t index, int reg)
                 return 0;
             }
         }
+    }
+
+    /*
+     * If the CPUID isn't included in env->features, QEMU-TDX will
+     * not enforce additional check even it also has some CPUID restrictions.
+     * e.g. CPUID[0xa]. View it as configurable and keep the values from KVM.
+     */
+    if (w == FEATURE_WORDS && i == tdx_caps->nr_cpuid_configs) {
+        ret = native;
     }
 
     return ret;