intel-staging/qemu-tdx

commit ed4e7569dd016203b567d530e6fd263d6ee5b6ae

target/i386: add SPR-TDX CPU model
Add a new CPU model named SapphireRapids-TDX. It is a template of CPUID
for the TDX on SPR platform. Some CPU Info like FMS and model id is
retrived from a stepping D SPR SDP.

The listed features all belong to the TDX-supported CPUID bits, including:
* Native supported ones.
* TDX fixed1 features.
* Configurable (if native) && KVM-supported features; (If not supported
by KVM, exposing uncondionally may bring unexpected behavior).

In addition, the legacy KVM-specific default features are not fit for
TDX CPU model, so skip the assignment in x86_cpu_load_model.

Signed-off-by: Chenyi Qiang <chenyi.qiang@intel.com>

target/i386/kvm/kvm-cpu.c
@@ -13,6 +13,7 @@
 #include "kvm-cpu.h"
 #include "qapi/error.h"
 #include "sysemu/sysemu.h"
+#include "sysemu/tdx.h"
 #include "hw/boards.h"
 
 #include "kvm_i386.h"
@@ -163,7 +164,7 @@ static void kvm_cpu_instance_init(CPUState *cs)
 
     if (xcc->model) {
         /* only applies to builtin_x86_defs cpus */
-        if (!kvm_irqchip_in_kernel()) {
+        if (!kvm_irqchip_in_kernel() && !kvm_tdx_enabled()) {
             x86_cpu_change_kvm_default("x2apic", "off");
         } else if (kvm_irqchip_is_split() && kvm_enable_x2apic()) {
             x86_cpu_change_kvm_default("kvm-msi-ext-dest-id", "on");

target/i386/cpu.c
@@ -3460,6 +3460,124 @@ static const X86CPUDefinition builtin_x86_defs[] = {
         }
     },
     {
+        .name = "SapphireRapids-TDX",
+        .level = 0x20,
+        .vendor = CPUID_VENDOR_INTEL,
+        .family = 6,
+        .model = 15,
+        .stepping = 3,
+        .features[FEAT_1_EDX] =
+            /* CPUID_CONFIG: CPUID_ACPI CPUID_HT CPUID_TM CPUID_PBE */
+            CPUID_FP87 | CPUID_VME | CPUID_DE | CPUID_PSE | CPUID_TSC |
+            CPUID_MSR | CPUID_PAE | CPUID_MCE | CPUID_CX8 | CPUID_APIC |
+            CPUID_SEP | CPUID_MTRR | CPUID_PGE | CPUID_MCA | CPUID_CMOV |
+            CPUID_PAT | CPUID_PSE36 | CPUID_CLFLUSH | CPUID_DTS |
+            0/* CPUID_ACPI */ | CPUID_MMX | CPUID_FXSR | CPUID_SSE |
+            CPUID_SSE2 | CPUID_SS | 0, /* CPUID_HT | CPUID_TM | CPUID_PBE */
+        .features[FEAT_1_ECX] =
+            /*
+             * CPUID_CONFIG: CPUID_EXT_EST CPUID_EXT_TM2 CPUID_EXT_XTPR
+             * XFAM: CPUID_EXT_FMA CPUID_EXT_AVX CPUID_EXT_F16C
+             */
+            CPUID_EXT_SSE3 | CPUID_EXT_PCLMULQDQ | CPUID_EXT_DTES64 |
+            CPUID_EXT_DSCPL | 0 /* CPUID_EXT_EST | CPUID_EXT_TM2 */ |
+            CPUID_EXT_SSSE3 | CPUID_EXT_SDBG | CPUID_EXT_FMA |
+            CPUID_EXT_CX16 | 0 /* CPUID_EXT_XTPR */ | CPUID_EXT_PDCM |
+            CPUID_EXT_PCID | CPUID_EXT_SSE41 | CPUID_EXT_SSE42 |
+            CPUID_EXT_X2APIC | CPUID_EXT_MOVBE | CPUID_EXT_POPCNT |
+            CPUID_EXT_TSC_DEADLINE_TIMER | CPUID_EXT_AES |
+            CPUID_EXT_XSAVE | CPUID_EXT_AVX | CPUID_EXT_F16C |
+            CPUID_EXT_RDRAND,
+        .features[FEAT_8000_0001_EDX] =
+            CPUID_EXT2_SYSCALL | CPUID_EXT2_NX | CPUID_EXT2_PDPE1GB |
+            CPUID_EXT2_RDTSCP | CPUID_EXT2_LM,
+        .features[FEAT_8000_0001_ECX] =
+            CPUID_EXT3_LAHF_LM | CPUID_EXT3_ABM |
+            CPUID_EXT3_3DNOWPREFETCH,
+        .features[FEAT_7_0_EBX] =
+            /*
+             * CPUID_CONFIG: CPUID_7_0_EBX_BMI1 CPUID_7_0_EBX_BMI2
+             *               CPUID_7_0_EBX_PQM CPUID_7_0_EBX_CQE
+             *               CPUID_7_0_EBX_ADX
+             * XFAM: CPUID_7_0_EBX_AVX2 CPUID_7_0_EBX_AVX512F
+             *       CPUID_7_0_EBX_AVX512DQ CPUID_7_0_EBX_AVX512IFMA
+             *       CPUID_7_0_EBX_INTEL_PT CPUID_7_0_EBX_AVX512CD
+             *       CPUID_7_0_EBX_AVX512BW CPUID_7_0_EBX_AVX512VL
+             *
+             *       CPUID_7_0_EBX_AVX512PF CPUID_7_0_EBX_AVX512ER not
+             *       supported on SPR-D
+             */
+            CPUID_7_0_EBX_FSGSBASE | CPUID_7_0_EBX_BMI1 |
+            CPUID_7_0_EBX_HLE | CPUID_7_0_EBX_AVX2 |
+            CPUID_7_0_EBX_FDP_EXCPTN_ONLY | CPUID_7_0_EBX_SMEP |
+            CPUID_7_0_EBX_BMI2 | CPUID_7_0_EBX_ERMS | CPUID_7_0_EBX_INVPCID |
+            CPUID_7_0_EBX_RTM | 0 /*CPUID_7_0_EBX_PQM */ |
+            CPUID_7_0_EBX_FCS_FDS_DEP | 0 /* CPUID_7_0_EBX_CQE */ |
+            CPUID_7_0_EBX_AVX512F | CPUID_7_0_EBX_AVX512DQ |
+            CPUID_7_0_EBX_RDSEED | CPUID_7_0_EBX_ADX |
+            CPUID_7_0_EBX_SMAP | CPUID_7_0_EBX_AVX512IFMA |
+            CPUID_7_0_EBX_CLFLUSHOPT | CPUID_7_0_EBX_CLWB |
+            0 /* CPUID_7_0_EBX_INTEL_PT */ | CPUID_7_0_EBX_AVX512CD |
+            CPUID_7_0_EBX_SHA_NI | CPUID_7_0_EBX_AVX512BW |
+            CPUID_7_0_EBX_AVX512VL,
+        .features[FEAT_7_0_ECX] =
+            /*
+             * CPUID_CONFIG: CPUID_7_0_ECX_WAITPKG CPUID_7_0_ECX_TME
+             * XFAM: CPUID_7_0_ECX_AVX512_VBMI CPUID_7_0_ECX_PKU
+             *        CPUID_7_0_ECX_AVX512_VBMI2 CPUID_7_0_ECX_CET_SHSTK
+             *        CPUID_7_0_ECX_VAES CPUID_7_0_ECX_VPCLMULQDQ
+             *        CPUID_7_0_ECX_AVX512VNNI CPUID_7_0_ECX_AVX512BITALG
+             *        CPUID_7_0_ECX_AVX512_VPOPCNTDQ
+             * ATTRIBUTE: CPUID_7_0_ECX_PKS
+             */
+            CPUID_7_0_ECX_AVX512_VBMI | CPUID_7_0_ECX_UMIP | CPUID_7_0_ECX_PKU |
+            CPUID_7_0_ECX_WAITPKG | CPUID_7_0_ECX_AVX512_VBMI2 |
+            0 /* CPUID_7_0_ECX_CET_SHSTK */ | CPUID_7_0_ECX_GFNI | CPUID_7_0_ECX_VAES |
+            CPUID_7_0_ECX_VPCLMULQDQ | CPUID_7_0_ECX_AVX512VNNI |
+            CPUID_7_0_ECX_AVX512BITALG | 0 /* CPUID_7_0_ECX_TME */ |
+            CPUID_7_0_ECX_AVX512_VPOPCNTDQ | CPUID_7_0_ECX_LA57 |
+            CPUID_7_0_ECX_RDPID | CPUID_7_0_ECX_BUS_LOCK_DETECT |
+            CPUID_7_0_ECX_CLDEMOTE | CPUID_7_0_ECX_MOVDIRI |
+            CPUID_7_0_ECX_MOVDIR64B,
+        .features[FEAT_7_0_EDX] =
+            /*
+             * CPUID_CONFIG: CPUID_7_0_EDX_PCONFIG
+             * XFAM: CPUID_7_0_EDX_ULI CPUID_7_0_EDX_ARCH_LBR
+             *       CPUID_7_0_EDX_AVX512_VP2INTERSECT
+             *       CPUID_7_0_EDX_AMX_BF16 CPUID_7_0_EDX_CET_IBT
+             *       CPUID_7_0_EDX_AMX_INT8 CPUID_7_0_EDX_AMX_TILE
+             *
+             *
+             * CPUID_7_0_EDX_AVX512_4VNNIW  CPUID_7_0_EDX_AVX512_4FMAPS
+             * not supported
+             */
+            CPUID_7_0_EDX_FSRM | 0 /* CPUID_7_0_EDX_ULI */ |
+            CPUID_7_0_EDX_AVX512_VP2INTERSECT | CPUID_7_0_EDX_MD_CLEAR |
+            CPUID_7_0_EDX_SERIALIZE | CPUID_7_0_EDX_TSX_LDTRK |
+            0 /* CPUID_7_0_EDX_PCONFIG | CPUID_7_0_EDX_ARCH_LBR |
+            CPUID_7_0_EDX_CET_IBT | CPUID_7_0_EDX_AMX_BF16 */ |
+            CPUID_7_0_EDX_AVX512_FP16 | 0 /* CPUID_7_0_EDX_AMX_TILE |
+            CPUID_7_0_EDX_AMX_INT8 */ | CPUID_7_0_EDX_SPEC_CTRL |
+            CPUID_7_0_EDX_STIBP | CPUID_7_0_EDX_ARCH_CAPABILITIES |
+            CPUID_7_0_EDX_CORE_CAPABILITY | CPUID_7_0_EDX_SPEC_CTRL_SSBD,
+        .features[FEAT_XSAVE] =
+            /*
+             * XFAM: CPUID_XSAVE_XFD depend on all of XFAM[]
+             */
+            CPUID_XSAVE_XSAVEOPT | CPUID_XSAVE_XSAVEC |
+            CPUID_XSAVE_XGETBV1 | CPUID_XSAVE_XSAVES,
+            /* CPUID_XSAVE_XFD */
+        .features[FEAT_6_EAX] =
+            CPUID_6_EAX_ARAT,
+        .features[FEAT_KVM] = (1ULL << KVM_FEATURE_NOP_IO_DELAY) |
+            (1ULL << KVM_FEATURE_PV_EOI) | (1ULL << KVM_FEATURE_PV_UNHALT) |
+            (1ULL << KVM_FEATURE_PV_TLB_FLUSH) | (1ULL << KVM_FEATURE_PV_SEND_IPI) |
+            (1ULL << KVM_FEATURE_POLL_CONTROL) | (1ULL << KVM_FEATURE_PV_SCHED_YIELD) |
+            (1ULL << KVM_FEATURE_ASYNC_PF_INT) | (1ULL << KVM_FEATURE_MSI_EXT_DEST_ID),
+        .xlevel = 0x80000008,
+        .model_id = "Genuine Intel(R) CPU $0000%@"
+    },
+    {
         .name = "Denverton",
         .level = 21,
         .vendor = CPUID_VENDOR_INTEL,

target/i386/cpu.h
@@ -639,6 +639,7 @@ typedef uint64_t FeatureWordArray[FEATURE_WORDS];
 #define CPUID_EXT_TM2      (1U << 8)
 #define CPUID_EXT_SSSE3    (1U << 9)
 #define CPUID_EXT_CID      (1U << 10)
+#define CPUID_EXT_SDBG     (1U << 11)
 #define CPUID_EXT_FMA      (1U << 12)
 #define CPUID_EXT_CX16     (1U << 13)
 #define CPUID_EXT_XTPR     (1U << 14)
@@ -749,6 +750,8 @@ typedef uint64_t FeatureWordArray[FEATURE_WORDS];
 #define CPUID_7_0_EBX_HLE               (1U << 4)
 /* Intel Advanced Vector Extensions 2 */
 #define CPUID_7_0_EBX_AVX2              (1U << 5)
+/* FDP_EXCPTN_ONLY */
+#define CPUID_7_0_EBX_FDP_EXCPTN_ONLY    (1U << 6)
 /* Supervisor-mode Execution Prevention */
 #define CPUID_7_0_EBX_SMEP              (1U << 7)
 /* 2nd Group of Advanced Bit Manipulation Extensions */
@@ -759,8 +762,14 @@ typedef uint64_t FeatureWordArray[FEATURE_WORDS];
 #define CPUID_7_0_EBX_INVPCID           (1U << 10)
 /* Restricted Transactional Memory */
 #define CPUID_7_0_EBX_RTM               (1U << 11)
+/* Intel RDT Monitor capability */
+#define CPUID_7_0_EBX_PQM               (1U << 12)
+/* Deprecated FPU CS and FPU DS */
+#define CPUID_7_0_EBX_FCS_FDS_DEP       (1U << 13)
 /* Memory Protection Extension */
 #define CPUID_7_0_EBX_MPX               (1U << 14)
+/* Intel RDT Allocation capability */
+#define CPUID_7_0_EBX_CQE               (1U << 15)
 /* AVX-512 Foundation */
 #define CPUID_7_0_EBX_AVX512F           (1U << 16)
 /* AVX-512 Doubleword & Quadword Instruction */
@@ -818,6 +827,8 @@ typedef uint64_t FeatureWordArray[FEATURE_WORDS];
 #define CPUID_7_0_ECX_AVX512VNNI        (1U << 11)
 /* Support for VPOPCNT[B,W] and VPSHUFBITQMB */
 #define CPUID_7_0_ECX_AVX512BITALG      (1U << 12)
+/* Intel TME */
+#define CPUID_7_0_ECX_TME               (1U << 13)
 /* POPCNT for vectors of DW/QW */
 #define CPUID_7_0_ECX_AVX512_VPOPCNTDQ  (1U << 14)
 /* Placeholder for bit 15 */
@@ -853,10 +864,14 @@ typedef uint64_t FeatureWordArray[FEATURE_WORDS];
 #define CPUID_7_0_EDX_ULI               (1U << 5)
 /* AVX512 Vector Pair Intersection to a Pair of Mask Registers */
 #define CPUID_7_0_EDX_AVX512_VP2INTERSECT (1U << 8)
+/* MD Clear supported */
+#define CPUID_7_0_EDX_MD_CLEAR          (1U << 10)
 /* SERIALIZE instruction */
 #define CPUID_7_0_EDX_SERIALIZE         (1U << 14)
 /* TSX Suspend Load Address Tracking instruction */
 #define CPUID_7_0_EDX_TSX_LDTRK         (1U << 16)
+/* PCONFIG */
+#define CPUID_7_0_EDX_PCONFIG           (1U << 18)
 /* Architectural LBR support */
 #define CPUID_7_0_EDX_ARCH_LBR          (1U << 19)
 /* CET IBT feature */
@@ -907,6 +922,7 @@ typedef uint64_t FeatureWordArray[FEATURE_WORDS];
 #define CPUID_XSAVE_XSAVEC     (1U << 1)
 #define CPUID_XSAVE_XGETBV1    (1U << 2)
 #define CPUID_XSAVE_XSAVES     (1U << 3)
+#define CPUID_XSAVE_XFD        (1U << 4)
 
 #define CPUID_6_EAX_ARAT       (1U << 2)
 

commit 217b79a0f90050ac1f8baebe5edaa1b826d4feeb

i386/kvm: Fix TDX CPU model default props
When specifying a CPU model, there are some default features setting
in kvm_default_props for legacy VM. However, these features are not fit
for TD guest, e.g. some PV features are not supported any more. Skip
applying these default props when TDX is enabled.

Fixes: ed4e7569dd("target/i386: add SPR-TDX CPU model")
Signed-off-by: Chenyi Qiang <chenyi.qiang@intel.com>

target/i386/kvm/kvm-cpu.c
@@ -174,14 +174,16 @@ static void kvm_cpu_instance_init(CPUState *cs)
 
     if (xcc->model) {
         /* only applies to builtin_x86_defs cpus */
-        if (!kvm_irqchip_in_kernel() && !kvm_tdx_enabled()) {
+        if (!kvm_irqchip_in_kernel()) {
             x86_cpu_change_kvm_default("x2apic", "off");
         } else if (kvm_irqchip_is_split() && kvm_enable_x2apic()) {
             x86_cpu_change_kvm_default("kvm-msi-ext-dest-id", "on");
         }
 
         /* Special cases not set in the X86CPUDefinition structs: */
-        x86_cpu_apply_props(cpu, kvm_default_props);
+        if (!kvm_tdx_enabled()) {
+            x86_cpu_apply_props(cpu, kvm_default_props);
+        }
     }
 
     if (cpu->max_features) {