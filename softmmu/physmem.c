intel-staging/qemu-tdx

commit 16ebe4043b17b3b0bb48808c018f86d6314dac2c

Implement MemoryDebugOps interface for encrypted guest
Also the new address_space_{read,write} debug helper
interfaces which can be invoked by vendor specific guest
memory debug assist/hooks to do guest RAM memory accesses
using the added MemoryRegion callbacks.

Yuan Yao:
    - Fixed fuzz_dma_read_cb() parameter issue for QEMU 5.2.91.
    - Move the caller of encrypted_memory_debug_ops into phymem.c
      as common callbacks for encrypted guests.
    - Adapted address_space_read_debug/address_space_wirte_rom_debug
      with new definition of MemoryRegion::ram_debug_ops;
    - Install the encrypted_memory_debug_ops/phymem.c for INTEL
      TD guest.
    - Refactor the too long subject line
    - Fill buffer to 0x0 if failed to read guest memory

Signed-off-by: Ashish Kalra <ashish.kalra@amd.com>
Signed-off-by: Yuan Yao <yuan.yao@intel.com>

softmmu/physmem.c
@@ -3276,6 +3276,97 @@ void cpu_physical_memory_unmap(void *buffer, hwaddr len,
 #define RCU_READ_UNLOCK(...)     rcu_read_unlock()
 #include "memory_ldst.c.inc"
 
+inline MemTxResult address_space_read_debug(AddressSpace *as, hwaddr addr,
+                                            MemTxAttrs attrs, void *ptr,
+                                            hwaddr len)
+{
+    uint64_t val;
+    MemoryRegion *mr;
+    hwaddr l = len;
+    hwaddr addr1;
+    MemTxResult result = MEMTX_OK;
+    bool release_lock = false;
+    uint8_t *buf = ptr;
+    uint8_t *ram_ptr;
+
+    for (;;) {
+        RCU_READ_LOCK_GUARD();
+        mr = address_space_translate(as, addr, &addr1, &l, false, attrs);
+        if (!memory_access_is_direct(mr, false)) {
+            /* I/O case */
+            release_lock |= prepare_mmio_access(mr);
+            l = memory_access_size(mr, l, addr1);
+            result |= memory_region_dispatch_read(mr, addr1, &val,
+                                                  size_memop(l), attrs);
+            stn_he_p(buf, l, val);
+        } else {
+            /* RAM case */
+            fuzz_dma_read_cb(addr, l, mr);
+            ram_ptr = qemu_ram_ptr_length(mr->ram_block, addr1, &l, false);
+            if (attrs.debug && mr->ram_debug_ops) {
+                if (mr->ram_debug_ops->read(buf, ram_ptr,
+                                            addr1, l,
+                                            attrs) < 0)
+                    memset(buf, 0, l);
+            } else {
+                memcpy(buf, ram_ptr, l);
+            }
+            result = MEMTX_OK;
+        }
+        if (release_lock) {
+            qemu_mutex_unlock_iothread();
+            release_lock = false;
+        }
+
+        len -= l;
+        buf += l;
+        addr += l;
+
+        if (!len) {
+            break;
+        }
+        l = len;
+    }
+    return result;
+}
+
+MemTxResult address_space_write_rom_debug(AddressSpace *as,
+                                          hwaddr addr,
+                                          MemTxAttrs attrs,
+                                          const void *ptr,
+                                          hwaddr len)
+{
+    hwaddr l;
+    uint8_t *ram_ptr;
+    hwaddr addr1;
+    MemoryRegion *mr;
+    const uint8_t *buf = ptr;
+
+    RCU_READ_LOCK_GUARD();
+    while (len > 0) {
+        l = len;
+        mr = address_space_translate(as, addr, &addr1, &l, true, attrs);
+
+        if (!(memory_region_is_ram(mr) ||
+              memory_region_is_romd(mr))) {
+            l = memory_access_size(mr, l, addr1);
+        } else {
+            /* ROM/RAM case */
+            ram_ptr = qemu_map_ram_ptr(mr->ram_block, addr1);
+            if (attrs.debug && mr->ram_debug_ops) {
+                mr->ram_debug_ops->write(ram_ptr, addr1, buf, l, attrs);
+            } else {
+                memcpy(ram_ptr, buf, l);
+            }
+            invalidate_and_set_dirty(mr, addr1, l);
+        }
+        len -= l;
+        buf += l;
+        addr += l;
+    }
+    return MEMTX_OK;
+}
+
 int64_t address_space_cache_init(MemoryRegionCache *cache,
                                  AddressSpace *as,
                                  hwaddr addr,
@@ -3467,6 +3558,33 @@ int cpu_memory_rw_debug(CPUState *cpu, target_ulong addr,
         addr += l;
     }
     return 0;
+}
+
+static MemTxResult address_space_encrypted_memory_read_debug(AddressSpace *as,
+                                                             hwaddr addr, MemTxAttrs attrs,
+                                                             void *ptr, hwaddr len)
+{
+    attrs.debug = 1;
+    return address_space_read_debug(as, addr, attrs, ptr, len);
+}
+
+
+static MemTxResult address_space_encrypted_rom_write_debug(AddressSpace *as,
+                                                           hwaddr addr, MemTxAttrs attrs,
+                                                           const void *ptr, hwaddr len)
+{
+    attrs.debug = 1;
+    return address_space_write_rom_debug(as, addr, attrs, ptr, len);
+}
+
+static const MemoryDebugOps encrypted_memory_debug_ops = {
+    .read = address_space_encrypted_memory_read_debug,
+    .write = address_space_encrypted_rom_write_debug,
+};
+
+void set_encrypted_memory_debug_ops(void)
+{
+    address_space_set_debug_ops(&encrypted_memory_debug_ops);
 }
 
 /*

include/exec/memory.h
@@ -2528,6 +2528,12 @@ MemTxResult address_space_write_rom(AddressSpace *as, hwaddr addr,
                                     MemTxAttrs attrs,
                                     const void *buf, hwaddr len);
 
+MemTxResult address_space_write_rom_debug(AddressSpace *as,
+                                          hwaddr addr,
+                                          MemTxAttrs attrs,
+                                          const void *ptr,
+                                          hwaddr len);
+
 /* address_space_ld*: load from an address space
  * address_space_st*: store to an address space
  *
@@ -2794,6 +2800,8 @@ extern const MemoryDebugOps *physical_memory_debug_ops;
 
 void address_space_set_debug_ops(const MemoryDebugOps *ops);
 
+void set_encrypted_memory_debug_ops(void);
+
 static inline bool memory_access_is_direct(MemoryRegion *mr, bool is_write)
 {
     if (is_write) {
@@ -2848,6 +2856,10 @@ MemTxResult address_space_read(AddressSpace *as, hwaddr addr,
     }
     return result;
 }
+
+MemTxResult address_space_read_debug(AddressSpace *as, hwaddr addr,
+                                     MemTxAttrs attrs, void *buf,
+                                     hwaddr len);
 
 /**
  * address_space_read_cached: read from a cached RAM region

target/i386/kvm/tdx.c
@@ -286,6 +286,7 @@ int tdx_kvm_init(ConfidentialGuestSupport *cgs, KVMState *s, Error **errp)
         kvm_vm_check_extension(s, KVM_CAP_ENCRYPT_MEMORY_DEBUG)) {
         kvm_setup_set_memory_region_debug_ops(s,
                                               kvm_encrypted_guest_set_memory_region_debug_ops);
+        set_encrypted_memory_debug_ops();
     }
 
     return 0;

commit cdac16e7471d5240bbe73861dda5a9370ccfed52

softmmu/physmem: Return MEMTX_ERROR when fail to do memory read/write
Also do more restrict checking to avoid crash when one only implements
read or write callback.

Fixes: 16ebe4043b("Implement MemoryDebugOps interface for encrypted guest ")
Signed-off-by: Yuan Yao <yuan.yao@intel.com>

softmmu/physmem.c
@@ -3316,14 +3316,14 @@ inline MemTxResult address_space_read_debug(AddressSpace *as, hwaddr addr,
             /* RAM case */
             fuzz_dma_read_cb(addr, l, mr);
             ram_ptr = qemu_ram_ptr_length(mr->ram_block, addr1, &l, false);
-            if (attrs.debug && mr->ram_debug_ops) {
-                mr->ram_debug_ops->read(buf, ram_ptr,
-                                        addr, l,
-                                        attrs);
+            if (attrs.debug && mr->ram_debug_ops && mr->ram_debug_ops->read) {
+                if (mr->ram_debug_ops->read(buf, ram_ptr,
+                                            addr, l,
+                                            attrs) < 0)
+                    result |= MEMTX_ERROR;
             } else {
                 memcpy(buf, ram_ptr, l);
             }
-            result = MEMTX_OK;
         }
         if (release_lock) {
             qemu_mutex_unlock_iothread();
@@ -3334,7 +3334,7 @@ inline MemTxResult address_space_read_debug(AddressSpace *as, hwaddr addr,
         buf += l;
         addr += l;
 
-        if (!len) {
+        if (!len || result != MEMTX_OK) {
             break;
         }
         l = len;
@@ -3353,6 +3353,7 @@ MemTxResult address_space_write_rom_debug(AddressSpace *as,
     hwaddr addr1;
     MemoryRegion *mr;
     const uint8_t *buf = ptr;
+    MemTxResult result = MEMTX_OK;
 
     RCU_READ_LOCK_GUARD();
     while (len > 0) {
@@ -3365,18 +3366,25 @@ MemTxResult address_space_write_rom_debug(AddressSpace *as,
         } else {
             /* ROM/RAM case */
             ram_ptr = qemu_map_ram_ptr(mr->ram_block, addr1);
-            if (attrs.debug && mr->ram_debug_ops) {
-                mr->ram_debug_ops->write(ram_ptr, addr, buf, l, attrs);
+            if (attrs.debug && mr->ram_debug_ops && mr->ram_debug_ops->write) {
+                if (mr->ram_debug_ops->write(ram_ptr,addr,
+                                             buf, l, attrs) < 0)
+                    result |= MEMTX_ERROR;
             } else {
                 memcpy(ram_ptr, buf, l);
             }
             invalidate_and_set_dirty(mr, addr1, l);
         }
+
+        if (result != MEMTX_ERROR) {
+            break;
+        }
+
         len -= l;
         buf += l;
         addr += l;
     }
-    return MEMTX_OK;
+    return result;
 }
 
 int64_t address_space_cache_init(MemoryRegionCache *cache,