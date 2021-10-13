intel-staging/qemu-tdx

commit c73e458845ed7ac05995c7e079e63dcc1183c3a1

Implement the common MemoryRegion::ram_debug_ops for encrypted guests
The new functions are added into target/i386/kvm/kvm.c as common functions
to support encrypted guest for KVM on x86.

Now we enable these only for INTEL TD guests.

Signed-off-by: Yuan Yao <yuan.yao@intel.com>

target/i386/kvm/kvm.c
@@ -2333,7 +2333,7 @@ int kvm_arch_init(MachineState *ms, KVMState *s)
         return ret;
     }
 
-    ret = tdx_kvm_init(ms->cgs, &local_err);
+    ret = tdx_kvm_init(ms->cgs, s, &local_err);
     if (ret < 0) {
         error_report_err(local_err);
         return ret;
@@ -5030,4 +5030,55 @@ bool kvm_has_waitpkg(void)
 bool kvm_arch_cpu_check_are_resettable(void)
 {
     return !sev_es_enabled();
+}
+
+static int kvm_encrypted_guest_read_memory(uint8_t *dest,
+                                           const uint8_t *hva_src, hwaddr gpa_src,
+                                           uint32_t len, MemTxAttrs attrs)
+{
+    struct kvm_rw_memory rw;
+
+    if (kvm_tdx_enabled()) {
+        rw.addr = gpa_src;
+    } else if (sev_es_enabled()) {
+        rw.addr = (__u64)hva_src;
+    } else {
+        return -EINVAL;
+    }
+
+    rw.ubuf = (__u64)dest;
+    rw.len = len;
+
+    return kvm_vm_ioctl(kvm_state, KVM_MEMORY_ENCRYPT_READ_MEMORY, &rw);
+}
+
+static int kvm_encrypted_guest_write_memory(uint8_t *hva_dest, hwaddr gpa_dest,
+                                            const uint8_t *src,
+                                            uint32_t len, MemTxAttrs attrs)
+{
+    struct kvm_rw_memory rw;
+
+    if (kvm_tdx_enabled()) {
+        rw.addr = gpa_dest;
+    } else if (sev_es_enabled()){
+        rw.addr = (__u64)hva_dest;
+    } else {
+        return -EINVAL;
+    }
+
+    rw.ubuf = (__u64)src;
+    rw.len = len;
+
+    return kvm_vm_ioctl(kvm_state, KVM_MEMORY_ENCRYPT_WRITE_MEMORY, &rw);
+}
+
+static MemoryRegionRAMReadWriteOps kvm_encrypted_guest_mr_debug_ops = {
+    .read = kvm_encrypted_guest_read_memory,
+    .write = kvm_encrypted_guest_write_memory,
+};
+
+void kvm_encrypted_guest_set_memory_region_debug_ops(void *handle,
+                                                     MemoryRegion *mr)
+{
+    memory_region_set_ram_debug_ops(mr, &kvm_encrypted_guest_mr_debug_ops);
 }

include/sysemu/kvm.h
@@ -552,4 +552,6 @@ void kvm_setup_set_memory_region_debug_ops(struct KVMState *s,
                                            set_memory_region_debug_ops new_ops);
 void kvm_set_memory_region_debug_ops(void *handle, MemoryRegion *mr);
 
+void kvm_encrypted_guest_set_memory_region_debug_ops(void *handle,
+                                                     MemoryRegion *mr);
 #endif

target/i386/kvm/tdx.c
@@ -259,7 +259,7 @@ static struct kvm_tdx_capabilities *tdx_caps = (void *)&__tdx_caps;
 #define XCR0_MASK (MAKE_64BIT_MASK(0, 8) | BIT_ULL(9) | MAKE_64BIT_MASK(17, 2))
 #define XSS_MASK (~XCR0_MASK)
 
-int tdx_kvm_init(ConfidentialGuestSupport *cgs, Error **errp)
+int tdx_kvm_init(ConfidentialGuestSupport *cgs, KVMState *s, Error **errp)
 {
     TdxGuest *tdx = (TdxGuest *)object_dynamic_cast(OBJECT(cgs),
                                                     TYPE_TDX_GUEST);
@@ -281,6 +281,12 @@ int tdx_kvm_init(ConfidentialGuestSupport *cgs, Error **errp)
     }
 
     qemu_add_machine_init_done_late_notifier(&tdx_machine_done_late_notify);
+
+    if (tdx->debug &&
+        kvm_vm_check_extension(s, KVM_CAP_ENCRYPT_MEMORY_DEBUG)) {
+        kvm_setup_set_memory_region_debug_ops(s,
+                                              kvm_encrypted_guest_set_memory_region_debug_ops);
+    }
 
     return 0;
 }

target/i386/kvm/tdx.h
@@ -51,7 +51,7 @@ typedef struct TdxGuest {
     TdxFirmware fw;
 } TdxGuest;
 
-int tdx_kvm_init(ConfidentialGuestSupport *cgs, Error **errp);
+int tdx_kvm_init(ConfidentialGuestSupport *cgs, KVMState *s, Error **errp);
 uint32_t tdx_get_cpuid_config(uint32_t function, uint32_t index, int reg);
 
 #endif

commit 9bb4f0bec989d8f5441a6afef1a033979e8e7e3e

i386/kvm: Resume the debug guest memory accessing after -EINTR
Also remove the memset() in framework level but leave it to into the
actual implementation.

Fixes: c73e458845 ("Implement the common MemoryRegion::ram_debug_ops for encrypted guests ")
Signed-off-by: Yuan Yao <yuan.yao@intel.com>

softmmu/physmem.c
@@ -3317,10 +3317,9 @@ inline MemTxResult address_space_read_debug(AddressSpace *as, hwaddr addr,
             fuzz_dma_read_cb(addr, l, mr);
             ram_ptr = qemu_ram_ptr_length(mr->ram_block, addr1, &l, false);
             if (attrs.debug && mr->ram_debug_ops) {
-                if (mr->ram_debug_ops->read(buf, ram_ptr,
-                                            addr, l,
-                                            attrs) < 0)
-                    memset(buf, 0, l);
+                mr->ram_debug_ops->read(buf, ram_ptr,
+                                        addr, l,
+                                        attrs);
             } else {
                 memcpy(buf, ram_ptr, l);
             }

target/i386/kvm/kvm.c
@@ -5050,6 +5050,8 @@ static int kvm_encrypted_guest_read_memory(uint8_t *dest,
                                            const uint8_t *hva_src, hwaddr gpa_src,
                                            uint32_t len, MemTxAttrs attrs)
 {
+    int ret;
+    uint64_t completed_len = 0;
     struct kvm_rw_memory rw;
 
     if (kvm_tdx_enabled()) {
@@ -5062,14 +5064,37 @@ static int kvm_encrypted_guest_read_memory(uint8_t *dest,
 
     rw.ubuf = (__u64)dest;
     rw.len = len;
+    for(;;) {
+        ret = kvm_vm_ioctl(kvm_state, KVM_MEMORY_ENCRYPT_READ_MEMORY, &rw);
+        if (ret >= 0)
+            break;
 
-    return kvm_vm_ioctl(kvm_state, KVM_MEMORY_ENCRYPT_READ_MEMORY, &rw);
+        completed_len += rw.len;
+        rw.addr += rw.len;
+        rw.ubuf += rw.len;
+        rw.len = len - completed_len;
+
+        if (ret != -EINTR)
+            break;
+    }
+
+    if (ret < 0) {
+        uint64_t total_fail_len;
+
+        total_fail_len = len > completed_len ? len - completed_len : len;
+        completed_len = len - total_fail_len;
+        memset(dest + completed_len, 0, total_fail_len);
+    }
+
+    return ret;
 }
 
 static int kvm_encrypted_guest_write_memory(uint8_t *hva_dest, hwaddr gpa_dest,
                                             const uint8_t *src,
                                             uint32_t len, MemTxAttrs attrs)
 {
+    int ret;
+    uint64_t completed_len = 0;
     struct kvm_rw_memory rw;
 
     if (kvm_tdx_enabled()) {
@@ -5082,8 +5107,20 @@ static int kvm_encrypted_guest_write_memory(uint8_t *hva_dest, hwaddr gpa_dest,
 
     rw.ubuf = (__u64)src;
     rw.len = len;
+    for(;;) {
+        ret = kvm_vm_ioctl(kvm_state, KVM_MEMORY_ENCRYPT_WRITE_MEMORY, &rw);
+        if (ret >= 0)
+            break;
+        if (ret != -EINTR)
+            break;
 
-    return kvm_vm_ioctl(kvm_state, KVM_MEMORY_ENCRYPT_WRITE_MEMORY, &rw);
+        completed_len += rw.len;
+        rw.addr += rw.len;
+        rw.ubuf += rw.len;
+        rw.len = len - completed_len;
+    }
+
+    return ret;
 }
 
 static MemoryRegionRAMReadWriteOps kvm_encrypted_guest_mr_debug_ops = {