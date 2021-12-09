ae55e1c7bcf8db9b66d1ca53a8ab6816fffe61d8
Xiaoyao Li <xiaoyao.li@intel.com>
2021-12-09 16:55:40 +0800

    i386: Introduce tdx-guest object
    Introduce tdx-guest object which inherits X86_CONFIDENTIAL_GUEST,
    and will be used to create TDX VMs (TDs) by
    
      qemu -machine ...,confidential-guest-support=tdx0	\
           -object tdx-guest,id=tdx0
    
    So far, it has no QAPI member/properety decleared and only one internal
    member 'attributes' with fixed value 0 that not configurable.
    
    QAPI properties will be added later.
    
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Acked-by: Gerd Hoffmann <kraxel@redhat.com>
    Acked-by: Markus Armbruster <armbru@redhat.com>
    ---
    Chanegs in v6:
     - Make tdx-guest inherits X86_CONFIDENTIAL_GUEST;
     - set cgs->require_guest_memfd;
     - update QAPI version to since 9.1;
    
    Changes in v4:
     - update the new qapi `since` filed from 8.2 to 9.0
    
    Changes in v1
     - make @attributes not user-settable

---
+++ target/i386/kvm/tdx.c
@@ -0,0 +1,42 @@
+/*
+ * QEMU TDX support
+ *
+ * Copyright Intel
+ *
+ * Author:
+ *      Xiaoyao Li <xiaoyao.li@intel.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory
+ *
+ */
+
+#include "qemu/osdep.h"
+#include "qom/object_interfaces.h"
+
+#include "tdx.h"
+
+/* tdx guest */
+OBJECT_DEFINE_TYPE_WITH_INTERFACES(TdxGuest,
+                                   tdx_guest,
+                                   TDX_GUEST,
+                                   X86_CONFIDENTIAL_GUEST,
+                                   { TYPE_USER_CREATABLE },
+                                   { NULL })
+
+static void tdx_guest_init(Object *obj)
+{
+    ConfidentialGuestSupport *cgs = CONFIDENTIAL_GUEST_SUPPORT(obj);
+    TdxGuest *tdx = TDX_GUEST(obj);
+
+    cgs->require_guest_memfd = true;
+    tdx->attributes = 0;
+}
+
+static void tdx_guest_finalize(Object *obj)
+{
+}
+
+static void tdx_guest_class_init(ObjectClass *oc, void *data)
+{
+}
