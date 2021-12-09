eac9c58ca89d1cfa76018f64d1e1c89ef8e119c2
Xiaoyao Li <xiaoyao.li@intel.com>
2021-12-09 16:55:40 +0800

    i386: Introduce tdx-guest object
    Introduce tdx-guest object which implements the interface of
    CONFIDENTIAL_GUEST_SUPPORT, and will be used to create TDX VMs (TDs) by
    
      qemu -machine ...,confidential-guest-support=tdx0	\
           -object tdx-guset,id=tdx0
    
    It has only one property 'attributes' with fixed value 0 and not
    configurable so far.
    
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Acked-by: Gerd Hoffmann <kraxel@redhat.com>
    ---
    changes from RFC-V4
    - make @attributes not user-settable

---
+++ target/i386/kvm/tdx.c
@@ -0,0 +1,40 @@
+/*
+ * QEMU TDX support
+ *
+ * Copyright Intel
+ *
+ * Author:
+ *      Xiaoyao Li <xiaoyao.li@intel.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory
+ *
+ */
+
+#include "qemu/osdep.h"
+#include "qom/object_interfaces.h"
+
+#include "tdx.h"
+
+/* tdx guest */
+OBJECT_DEFINE_TYPE_WITH_INTERFACES(TdxGuest,
+                                   tdx_guest,
+                                   TDX_GUEST,
+                                   CONFIDENTIAL_GUEST_SUPPORT,
+                                   { TYPE_USER_CREATABLE },
+                                   { NULL })
+
+static void tdx_guest_init(Object *obj)
+{
+    TdxGuest *tdx = TDX_GUEST(obj);
+
+    tdx->attributes = 0;
+}
+
+static void tdx_guest_finalize(Object *obj)
+{
+}
+
+static void tdx_guest_class_init(ObjectClass *oc, void *data)
+{
+}
