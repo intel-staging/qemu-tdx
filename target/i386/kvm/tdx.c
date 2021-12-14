d64d0075c941ae1b607119ff5cdc6b3c93ec315a
Xiaoyao Li <xiaoyao.li@intel.com>
2021-12-14 13:49:25 +0800

    i386/tdx: Introduce is_tdx_vm() helper and cache tdx_guest object
    It will need special handling for TDX VMs all around the QEMU.
    Introduce is_tdx_vm() helper to query if it's a TDX VM.
    
    Cache tdx_guest object thus no need to cast from ms->cgs every time.
    
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Acked-by: Gerd Hoffmann <kraxel@redhat.com>
    Reviewed-by: Isaku Yamahata <isaku.yamahata@intel.com>
    ---
    changes in v3:
    - replace object_dynamic_cast with TDX_GUEST();

--- target/i386/kvm/tdx.c
+++ target/i386/kvm/tdx.c
@@ -21,7 +21,15 @@
 #include "kvm_i386.h"
 #include "tdx.h"
 
+static TdxGuest *tdx_guest;
+
 static struct kvm_tdx_capabilities *tdx_caps;
+
+/* It's valid after kvm_arch_init()->kvm_tdx_init() */
+bool is_tdx_vm(void)
+{
+    return !!tdx_guest;
+}
 
 enum tdx_ioctl_level{
     TDX_VM_IOCTL,
@@ -104,15 +112,20 @@ static int get_tdx_capabilities(Error **errp)
 static int tdx_kvm_init(ConfidentialGuestSupport *cgs, Error **errp)
 {
     MachineState *ms = MACHINE(qdev_get_machine());
+    TdxGuest *tdx = TDX_GUEST(cgs);
     int r = 0;
 
     ms->require_guest_memfd = true;
 
     if (!tdx_caps) {
         r = get_tdx_capabilities(errp);
+        if (r) {
+            return r;
+        }
     }
 
-    return r;
+    tdx_guest = tdx;
+    return 0;
 }
 
 /* tdx guest */
