db07e4c32b91866c6a97d0a49758432fa08adb88
Chao Peng <chao.p.peng@linux.intel.com>
2022-01-10 18:21:41 +0800

    headers: fix KVM_MEMORY_ENCRYPT_READ_MEMORY
    Signed-off-by: Chao Peng <chao.p.peng@linux.intel.com>

--- linux-headers/linux/kvm.h
+++ linux-headers/linux/kvm.h
@@ -1797,6 +1797,10 @@ struct kvm_xen_vcpu_attr {
 #define KVM_XEN_VCPU_ATTR_TYPE_RUNSTATE_DATA	0x4
 #define KVM_XEN_VCPU_ATTR_TYPE_RUNSTATE_ADJUST	0x5
 
+/* Read/write encrypted guest memory, for guest debugging support in QEMU*/
+#define KVM_MEMORY_ENCRYPT_READ_MEMORY  _IOW(KVMIO, 0xcc, struct kvm_rw_memory)
+#define KVM_MEMORY_ENCRYPT_WRITE_MEMORY _IOW(KVMIO, 0xcd, struct kvm_rw_memory)
+
 /* Secure Encrypted Virtualization command */
 enum sev_cmd_id {
 	/* Guest initialization commands */
