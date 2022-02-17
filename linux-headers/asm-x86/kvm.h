0b9ea928bbe68aad8d988bad138654d7fd0a5a3a
Isaku Yamahata <isaku.yamahata@intel.com>
2022-02-17 07:16:23 -0800

    fixup! linux-headers: Update headers to pull in TDX API changes

--- linux-headers/asm-x86/kvm.h
+++ linux-headers/asm-x86/kvm.h
@@ -503,7 +503,7 @@ struct kvm_pmu_event_filter {
 #define KVM_PMU_EVENT_ALLOW 0
 #define KVM_PMU_EVENT_DENY 1
 
-#define KVM_X86_LEGACY_VM	0
+#define KVM_X86_DEFAULT_VM	0
 #define KVM_X86_TDX_VM		1
 
 /* Trust Domain eXtension command*/
