4dc0aabacb5d84746460a712b3a96ca50b50f010
Xiaoyao Li <xiaoyao.li@intel.com>
2022-02-18 10:37:57 +0800

    i386/tdx: Wire CPU features up with attributes of TD guest
    For QEMU VMs, PKS is configured via CPUID_7_0_ECX_PKS and PMU is
    configured by x86cpu->enable_pmu. Reuse the existing configuration
    interface for TDX VMs.
    
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Acked-by: Gerd Hoffmann <kraxel@redhat.com>

--- target/i386/kvm/tdx.c
+++ target/i386/kvm/tdx.c
@@ -33,6 +33,8 @@
                                      (1U << KVM_FEATURE_MSI_EXT_DEST_ID))
 
 #define TDX_TD_ATTRIBUTES_SEPT_VE_DISABLE   BIT_ULL(28)
+#define TDX_TD_ATTRIBUTES_PKS               BIT_ULL(30)
+#define TDX_TD_ATTRIBUTES_PERFMON           BIT_ULL(63)
 
 #define TDX_ATTRIBUTES_MAX_BITS      64
 
@@ -477,6 +479,15 @@ static int tdx_kvm_init(ConfidentialGuestSupport *cgs, Error **errp)
     return 0;
 }
 
+static void setup_td_guest_attributes(X86CPU *x86cpu)
+{
+    CPUX86State *env = &x86cpu->env;
+
+    tdx_guest->attributes |= (env->features[FEAT_7_0_ECX] & CPUID_7_0_ECX_PKS) ?
+                             TDX_TD_ATTRIBUTES_PKS : 0;
+    tdx_guest->attributes |= x86cpu->enable_pmu ? TDX_TD_ATTRIBUTES_PERFMON : 0;
+}
+
 int tdx_pre_create_vcpu(CPUState *cpu, Error **errp)
 {
     MachineState *ms = MACHINE(qdev_get_machine());
@@ -498,6 +509,8 @@ int tdx_pre_create_vcpu(CPUState *cpu, Error **errp)
         error_setg(errp, "Unable to set MAX VCPUS to %d", ms->smp.cpus);
         return r;
     }
+
+    setup_td_guest_attributes(x86cpu);
 
     init_vm->cpuid.nent = kvm_x86_arch_cpuid(env, init_vm->cpuid.entries, 0);
 
