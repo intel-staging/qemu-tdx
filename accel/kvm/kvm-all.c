intel-staging/qemu-tdx

commit e96ea07a99a0c770102db7087c08fb9f430d1e97

kvm: Enable KVM_MEM_PRIVATE memslot
Signed-off-by: Chao Peng <chao.p.peng@linux.intel.com>

accel/kvm/kvm-all.c
@@ -361,34 +361,40 @@ int kvm_physical_memory_addr_from_host(KVMState *s, void *ram,
 static int kvm_set_user_memory_region(KVMMemoryListener *kml, KVMSlot *slot, bool new)
 {
     KVMState *s = kvm_state;
-    struct kvm_userspace_memory_region mem;
+    struct kvm_userspace_memory_region_ext mem;
     int ret;
 
-    mem.slot = slot->slot | (kml->as_id << 16);
-    mem.guest_phys_addr = slot->start_addr;
-    mem.userspace_addr = (unsigned long)slot->ram;
-    mem.flags = slot->flags;
+    mem.region.slot = slot->slot | (kml->as_id << 16);
+    mem.region.guest_phys_addr = slot->start_addr;
+    mem.region.userspace_addr = (unsigned long)slot->ram;
+    mem.region.flags = slot->flags;
+    if (slot->flags | KVM_MEM_PRIVATE) {
+        mem.private_fd = slot->fd;
+        mem.private_offset = slot->ofs;
+    }
 
-    if (slot->memory_size && !new && (mem.flags ^ slot->old_flags) & KVM_MEM_READONLY) {
+    if (slot->memory_size && !new &&
+        (slot->flags ^ slot->old_flags) & KVM_MEM_READONLY) {
         /* Set the slot size to 0 before setting the slot to the desired
          * value. This is needed based on KVM commit 75d61fbc. */
-        mem.memory_size = 0;
+        mem.region.memory_size = 0;
         ret = kvm_vm_ioctl(s, KVM_SET_USER_MEMORY_REGION, &mem);
         if (ret < 0) {
             goto err;
         }
     }
-    mem.memory_size = slot->memory_size;
+    mem.region.memory_size = slot->memory_size;
     ret = kvm_vm_ioctl(s, KVM_SET_USER_MEMORY_REGION, &mem);
-    slot->old_flags = mem.flags;
+    slot->old_flags = mem.region.flags;
 err:
-    trace_kvm_set_user_memory(mem.slot, mem.flags, mem.guest_phys_addr,
-                              mem.memory_size, mem.userspace_addr, ret);
+    trace_kvm_set_user_memory(mem.region.slot, mem.region.flags,
+                              mem.region.guest_phys_addr, mem.region.memory_size,
+                              mem.region.userspace_addr, ret);
     if (ret < 0) {
         error_report("%s: KVM_SET_USER_MEMORY_REGION failed, slot=%d,"
                      " start=0x%" PRIx64 ", size=0x%" PRIx64 ": %s",
-                     __func__, mem.slot, slot->start_addr,
-                     (uint64_t)mem.memory_size, strerror(errno));
+                     __func__, mem.region.slot, slot->start_addr,
+                     (uint64_t)mem.region.memory_size, strerror(errno));
     }
     return ret;
 }
@@ -547,6 +553,9 @@ static int kvm_mem_flags(MemoryRegion *mr)
     }
     if (readonly && kvm_readonly_mem_allowed) {
         flags |= KVM_MEM_READONLY;
+    }
+    if (mr->ram_block && mr->ram_block->private_fd > 0) {
+        flags |= KVM_MEM_PRIVATE;
     }
     return flags;
 }
@@ -1442,6 +1451,10 @@ static void kvm_set_phys_mem(KVMMemoryListener *kml,
         mem->ram_start_offset = ram_start_offset;
         mem->ram = ram;
         mem->flags = kvm_mem_flags(mr);
+        if (mem->flags | KVM_MEM_PRIVATE) {
+            mem->fd = mr->ram_block->private_fd;
+	    mem->ofs = (uint8_t*)ram - mr->ram_block->host;
+        }
         kvm_slot_init_dirty_bitmap(mem);
         err = kvm_set_user_memory_region(kml, mem, true);
         if (err) {

include/sysemu/kvm_int.h
@@ -28,6 +28,8 @@ typedef struct KVMSlot
     int as_id;
     /* Cache of the offset in ram address space */
     ram_addr_t ram_start_offset;
+    int fd;
+    hwaddr ofs;
 } KVMSlot;
 
 typedef struct KVMMemoryListener {

commit e4e40e73bc84d336fb2e60af1efc70676208ebbc

KVM: fix KVM_MEM_PRIVATE flags handling
The patch of "kvm: Enable KVM_MEM_PRIVATE memslot" check KVM_MEM_PRIVATE
by "if (flags | KVM_MEM_PRIVATE)".  It should be "flags & KVM_MEM_PRIVATE".

Fixes: e96ea07a99 ("kvm: Enable KVM_MEM_PRIVATE memslot")
Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

accel/kvm/kvm-all.c
@@ -368,9 +368,12 @@ static int kvm_set_user_memory_region(KVMMemoryListener *kml, KVMSlot *slot, boo
     mem.region.guest_phys_addr = slot->start_addr;
     mem.region.userspace_addr = (unsigned long)slot->ram;
     mem.region.flags = slot->flags;
-    if (slot->flags | KVM_MEM_PRIVATE) {
+    if (slot->flags & KVM_MEM_PRIVATE) {
         mem.private_fd = slot->fd;
         mem.private_offset = slot->ofs;
+    } else {
+        mem.private_fd = -1;
+        mem.private_offset = -1;
     }
 
     if (slot->memory_size && !new &&
@@ -1451,9 +1454,12 @@ static void kvm_set_phys_mem(KVMMemoryListener *kml,
         mem->ram_start_offset = ram_start_offset;
         mem->ram = ram;
         mem->flags = kvm_mem_flags(mr);
-        if (mem->flags | KVM_MEM_PRIVATE) {
+        if (mem->flags & KVM_MEM_PRIVATE) {
             mem->fd = mr->ram_block->private_fd;
-	    mem->ofs = (uint8_t*)ram - mr->ram_block->host;
+            mem->ofs = (uint8_t*)ram - mr->ram_block->host;
+        } else {
+            mem->fd = -1;
+            mem->ofs = -1;
         }
         kvm_slot_init_dirty_bitmap(mem);
         err = kvm_set_user_memory_region(kml, mem, true);