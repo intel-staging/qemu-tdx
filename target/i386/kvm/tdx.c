0e091b1dc3bcabd0ada8e3931760cbfb5c5e0f3b
Xiaoyao Li <xiaoyao.li@intel.com>
2022-04-19 09:58:06 +0800

    i386/tdx: Track mem_ptr for each firmware entry of TDVF
    For each TDVF sections, QEMU needs to copy the content to guest
    private memory via KVM API (KVM_TDX_INIT_MEM_REGION).
    
    Introduce a field @mem_ptr for TdxFirmwareEntry to track the memory
    pointer of each TDVF sections. So that QEMU can add/copy them to guest
    private memory later.
    
    TDVF sections can be classified into two groups:
     - Firmware itself, e.g., TDVF BFV and CFV, that located separately from
       guest RAM. Its memory pointer is the bios pointer.
    
     - Sections located at guest RAM, e.g., TEMP_MEM and TD_HOB.
       mmap a new memory range for them.
    
    Register a machine_init_done callback to do the stuff.
    
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Acked-by: Gerd Hoffmann <kraxel@redhat.com>

--- target/i386/kvm/tdx.c
+++ target/i386/kvm/tdx.c
@@ -12,12 +12,15 @@
  */
 
 #include "qemu/osdep.h"
+#include "qemu/mmap-alloc.h"
 #include "qapi/error.h"
 #include "qom/object_interfaces.h"
 #include "standard-headers/asm-x86/kvm_para.h"
 #include "sysemu/kvm.h"
+#include "sysemu/sysemu.h"
 
 #include "hw/i386/x86.h"
+#include "hw/i386/tdvf.h"
 #include "kvm_i386.h"
 #include "tdx.h"
 #include "../cpu-internal.h"
@@ -450,6 +453,33 @@ static void update_tdx_cpuid_lookup_by_tdx_caps(void)
             (tdx_caps->xfam_fixed1 & CPUID_XSTATE_XSS_MASK) >> 32;
 }
 
+static void tdx_finalize_vm(Notifier *notifier, void *unused)
+{
+    TdxFirmware *tdvf = &tdx_guest->tdvf;
+    TdxFirmwareEntry *entry;
+
+    for_each_tdx_fw_entry(tdvf, entry) {
+        switch (entry->type) {
+        case TDVF_SECTION_TYPE_BFV:
+        case TDVF_SECTION_TYPE_CFV:
+            entry->mem_ptr = tdvf->mem_ptr + entry->data_offset;
+            break;
+        case TDVF_SECTION_TYPE_TD_HOB:
+        case TDVF_SECTION_TYPE_TEMP_MEM:
+            entry->mem_ptr = qemu_ram_mmap(-1, entry->size,
+                                           qemu_real_host_page_size(), 0, 0);
+            break;
+        default:
+            error_report("Unsupported TDVF section %d", entry->type);
+            exit(1);
+        }
+    }
+}
+
+static Notifier tdx_machine_done_notify = {
+    .notify = tdx_finalize_vm,
+};
+
 int tdx_kvm_init(MachineState *ms, Error **errp)
 {
     TdxGuest *tdx = (TdxGuest *)object_dynamic_cast(OBJECT(ms->cgs),
@@ -469,6 +499,8 @@ int tdx_kvm_init(MachineState *ms, Error **errp)
      * Thus, just mark readonly memory not supported for simplicity.
      */
     kvm_readonly_mem_allowed = false;
+
+    qemu_add_machine_init_done_notifier(&tdx_machine_done_notify);
 
     tdx_guest = tdx;
 
