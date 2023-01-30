6f27eb794e9048dc79e17efc50888b133422a000
Yuan Yao <yuan.yao@intel.com>
2023-01-30 14:10:31 +0800

    softmmu/physmem: Implements virtual memory accessing for encrypted guest
    Uses new introduced physical memory debug interface in
    cpu_memory_rw_debug() for guest physcial memory accessing
    after VA -> PA conversion.
    
    Signed-off-by: Yuan Yao <yuan.yao@intel.com>

--- softmmu/physmem.c
+++ softmmu/physmem.c
@@ -3459,6 +3459,8 @@ int cpu_memory_rw_debug(CPUState *cpu, vaddr addr,
         page = addr & TARGET_PAGE_MASK;
         phys_addr = cpu_get_phys_page_attrs_debug(cpu, page, &attrs);
         asidx = cpu_asidx_from_attrs(cpu, attrs);
+        /* set debug attrs to indicate memory access is from the debugger */
+        attrs.debug = 1;
 
         /* if no physical page mapped, return an error */
         if (phys_addr == -1)
@@ -3468,11 +3470,14 @@ int cpu_memory_rw_debug(CPUState *cpu, vaddr addr,
             l = len;
         phys_addr += (addr & ~TARGET_PAGE_MASK);
         if (is_write) {
-            res = address_space_write_rom(cpu->cpu_ases[asidx].as, phys_addr,
-                                          attrs, buf, l);
+            res = physical_memory_debug_ops->write(cpu->cpu_ases[asidx].as,
+                                                   phys_addr,
+                                                   attrs, buf, l, true);
+
         } else {
-            res = address_space_read(cpu->cpu_ases[asidx].as, phys_addr,
-                                     attrs, buf, l);
+            res = physical_memory_debug_ops->read(cpu->cpu_ases[asidx].as,
+                                                  phys_addr,
+                                                  attrs, buf, l);
         }
         if (res != MEMTX_OK) {
             return -1;
