0e363c4289d7fd0bc231fa1f51107ebeda79a507
Isaku Yamahata <isaku.yamahata@intel.com>
2023-03-30 16:36:27 -0700

    i386/tdx: Get tdx module metadata from sysfs
    TDX module has configuration metadata which provides information about
    configuration of guest TD.  Those information is needed to create guest.
    As those information is provided via sysfs, get them from sysfs.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- target/i386/kvm/tdx.c
+++ target/i386/kvm/tdx.c
@@ -53,6 +53,44 @@
 
 #define TDX_ATTRIBUTES_MAX_BITS      64
 
+/* TDX metadata base field id. */
+#define TDX_METADATA_ATTRIBUTES_FIXED0          0x1900000300000000ULL
+#define TDX_METADATA_ATTRIBUTES_FIXED1          0x1900000300000001ULL
+#define TDX_METADATA_XFAM_FIXED0                0x1900000300000002ULL
+#define TDX_METADATA_XFAM_FIXED1                0x1900000300000003ULL
+#define TDX_METADATA_NUM_CPUID_CONFIG           0x9900000100000004ULL
+#define TDX_METADATA_CPUID_LEAVES               0x9900000300000400ULL
+#define TDX_METADATA_CPUID_VALUES               0x9900000300000500ULL
+
+#define TDX_METADATA_SYSFS_PATH "/sys/firmware/tdx/tdx_module/metadata/%" PRIx64
+
+struct tdx_cpuid_config_leaf {
+    uint32_t leaf;
+    uint32_t sub_leaf;
+} QEMU_PACKED;
+
+struct tdx_cpuid_config_value {
+    uint32_t eax;
+    uint32_t ebx;
+    uint32_t ecx;
+    uint32_t edx;
+} QEMU_PACKED;
+
+struct tdx_cpuid_config {
+    struct tdx_cpuid_config_leaf leaf;
+    struct tdx_cpuid_config_value value;
+};
+
+struct tdx_capabilities {
+    uint64_t attrs_fixed0;
+    uint64_t attrs_fixed1;
+    uint64_t xfam_fixed0;
+    uint64_t xfam_fixed1;
+
+    uint16_t nr_cpuid_configs;
+    struct tdx_cpuid_config cpuid_configs[];
+};
+
 static FeatureMask tdx_attrs_ctrl_fields[TDX_ATTRIBUTES_MAX_BITS] = {
     [30] = { .index = FEAT_7_0_ECX, .mask = CPUID_7_0_ECX_PKS },
     [31] = { .index = FEAT_7_0_ECX, .mask = CPUID_7_0_ECX_KeyLocker},
@@ -267,7 +305,7 @@ static KvmTdxCpuidLookup tdx_cpuid_lookup[FEATURE_WORDS] = {
 
 static TdxGuest *tdx_guest;
 
-static struct kvm_tdx_capabilities *tdx_caps;
+static struct tdx_capabilities *tdx_caps;
 
 /* It's valid after kvm_confidential_guest_init()->kvm_tdx_init() */
 bool is_tdx_vm(void)
@@ -303,7 +341,7 @@ static inline uint32_t host_cpuid_reg(uint32_t function,
 static inline uint32_t tdx_cap_cpuid_config(uint32_t function,
                                             uint32_t index, int reg)
 {
-    struct kvm_tdx_cpuid_config *cpuid_c;
+    struct tdx_cpuid_config *cpuid_c;
     int ret = 0;
     int i;
 
@@ -314,20 +352,20 @@ static inline uint32_t tdx_cap_cpuid_config(uint32_t function,
     for (i = 0; i < tdx_caps->nr_cpuid_configs; i++) {
         cpuid_c = &tdx_caps->cpuid_configs[i];
         /* 0xffffffff in sub_leaf means the leaf doesn't require a sublesf */
-        if (cpuid_c->leaf == function &&
-            (cpuid_c->sub_leaf == 0xffffffff || cpuid_c->sub_leaf == index)) {
+        if (cpuid_c->leaf.leaf == function &&
+            (cpuid_c->leaf.sub_leaf == 0xffffffff || cpuid_c->leaf.sub_leaf == index)) {
             switch (reg) {
             case R_EAX:
-                ret = cpuid_c->eax;
+                ret = cpuid_c->value.eax;
                 break;
             case R_EBX:
-                ret = cpuid_c->ebx;
+                ret = cpuid_c->value.ebx;
                 break;
             case R_ECX:
-                ret = cpuid_c->ecx;
+                ret = cpuid_c->value.ecx;
                 break;
             case R_EDX:
-                ret = cpuid_c->edx;
+                ret = cpuid_c->value.edx;
                 break;
             default:
                 return 0;
@@ -625,12 +663,123 @@ static inline int tdx_vcpu_ioctl(void *vcpu_fd, int cmd_id, __u32 flags,
     return  __tdx_ioctl(vcpu_fd, TDX_VCPU_IOCTL, cmd_id, flags, data);
 }
 
+static int tdx_sysfs_read(uint64_t field_id, void *data, size_t count)
+{
+    int err = 0;
+    char *path;
+    int fd;
+    ssize_t r;
+
+    path = g_strdup_printf(TDX_METADATA_SYSFS_PATH, field_id);
+    fd = open(path, 0);
+    if (fd < 0) {
+        err = -errno;
+        goto out_free;
+    }
+
+    do {
+        r = read(fd, data, count);
+    } while (r == -1 && errno == EINTR);
+    if (r < 0) {
+        err = -errno;
+        goto out;
+    }
+
+    if (r != count) {
+        /* As count is small, e.g. 96, single read should serve all data. */
+        err = -EIO;
+    }
+out:
+    close(fd);
+out_free:
+    g_free(path);
+    return err;
+}
+
+static int get_tdx_capabilities_by_sysfs(void)
+{
+    int r;
+    uint16_t nr_cpuid_configs;
+    struct tdx_cpuid_config_leaf *leaves;
+    struct tdx_cpuid_config_value *values;
+    int i;
+
+    r = tdx_sysfs_read(TDX_METADATA_NUM_CPUID_CONFIG, &nr_cpuid_configs,
+                       sizeof(nr_cpuid_configs));
+    if (r < 0) {
+        return r;
+    }
+
+    tdx_caps = g_malloc(sizeof(*tdx_caps) +
+                        nr_cpuid_configs * sizeof(tdx_caps->cpuid_configs[0]));
+    leaves = g_malloc_n(nr_cpuid_configs, sizeof(*leaves));
+    values = g_malloc_n(nr_cpuid_configs, sizeof(*values));
+
+    tdx_caps->nr_cpuid_configs = nr_cpuid_configs;
+
+#define SYSFS(_field_id, member)        \
+    {                                   \
+        .field_id = _field_id,          \
+        .data = &member,                \
+        .size = sizeof(member),         \
+    }
+
+    struct {
+        uint64_t field_id;
+        void *data;
+        size_t size;
+    } sysfs[] = {
+        SYSFS(TDX_METADATA_ATTRIBUTES_FIXED0, tdx_caps->attrs_fixed0),
+        SYSFS(TDX_METADATA_ATTRIBUTES_FIXED1, tdx_caps->attrs_fixed1),
+        SYSFS(TDX_METADATA_XFAM_FIXED0, tdx_caps->xfam_fixed0),
+        SYSFS(TDX_METADATA_XFAM_FIXED0, tdx_caps->xfam_fixed0),
+        {
+            .field_id = TDX_METADATA_CPUID_LEAVES,
+            .data = leaves,
+            .size = nr_cpuid_configs * sizeof(*leaves),
+        },
+        {
+            .field_id = TDX_METADATA_CPUID_VALUES,
+            .data = values,
+            .size = nr_cpuid_configs * sizeof(*values),
+        },
+    };
+#undef SYSFS
+
+    for (i =0; i < ARRAY_SIZE(sysfs); i++) {
+        r = tdx_sysfs_read(sysfs[i].field_id, sysfs[i].data, sysfs[i].size);
+        if (r < 0) {
+            goto out;
+        }
+    }
+
+    for (i = 0; i < nr_cpuid_configs; i++) {
+        tdx_caps->cpuid_configs[i].leaf = leaves[i];
+        tdx_caps->cpuid_configs[i].value = values[i];
+    }
+
+    g_free(leaves);
+    g_free(values);
+    return 0;
+
+out:
+    g_free(leaves);
+    g_free(values);
+    g_free(tdx_caps);
+    tdx_caps = NULL;
+    return r;
+}
+
 static void get_tdx_capabilities(void)
 {
     struct kvm_tdx_capabilities *caps;
     /* 1st generation of TDX reports 6 cpuid configs */
     int nr_cpuid_configs = 6;
-    int r, size;
+    int r, size, i;
+
+    if (!get_tdx_capabilities_by_sysfs())
+        return;
+    warn_report("KVM TDX sysfs doesn't seem suported. fallback to the old ioctl");
 
     do {
         size = sizeof(struct kvm_tdx_capabilities) +
@@ -654,7 +803,25 @@ static void get_tdx_capabilities(void)
     }
     while (r == -E2BIG);
 
-    tdx_caps = caps;
+
+    tdx_caps = g_malloc(sizeof(*tdx_caps) +
+                        caps->nr_cpuid_configs * sizeof(tdx_caps->cpuid_configs[0]));
+    tdx_caps->attrs_fixed0 = caps->attrs_fixed0;
+    tdx_caps->attrs_fixed1 = caps->attrs_fixed1;
+    tdx_caps->xfam_fixed0 = caps->xfam_fixed0;
+    tdx_caps->xfam_fixed1 = caps->xfam_fixed1;
+    tdx_caps->nr_cpuid_configs = caps->nr_cpuid_configs;
+    for (i = 0; i < tdx_caps->nr_cpuid_configs; i++) {
+        struct kvm_tdx_cpuid_config *kvm = &caps->cpuid_configs[i];
+        struct tdx_cpuid_config *dest = &tdx_caps->cpuid_configs[i];
+
+        dest->leaf.leaf = kvm->leaf;
+        dest->leaf.sub_leaf = kvm->sub_leaf;
+        dest->value.eax = kvm->eax;
+        dest->value.ebx = kvm->ebx;
+        dest->value.ecx = kvm->ecx;
+        dest->value.edx = kvm->edx;
+    }
 }
 
 static void update_tdx_cpuid_lookup_by_tdx_caps(void)
@@ -991,7 +1158,7 @@ static int tdx_validate_attributes(TdxGuest *tdx)
 {
     if (((tdx->attributes & tdx_caps->attrs_fixed0) | tdx_caps->attrs_fixed1) !=
         tdx->attributes) {
-            error_report("Invalid attributes 0x%lx for TDX VM (fixed0 0x%llx, fixed1 0x%llx)",
+            error_report("Invalid attributes 0x%lx for TDX VM (fixed0 0x%lx, fixed1 0x%lx)",
                           tdx->attributes, tdx_caps->attrs_fixed0, tdx_caps->attrs_fixed1);
             return -EINVAL;
     }
