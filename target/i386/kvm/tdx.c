d2f73b400760016944d7904e8d2620e15755bdf3
Chao Peng <chao.p.peng@linux.intel.com>
2023-06-02 18:36:53 -0700

    i386/tdx: load TDVF for TD guest
    TDVF(OVMF) needs to run at private memory for TD guest. TDX cannot
    support pflash device since it doesn't support read-only private memory.
    Thus load TDVF(OVMF) with -bios option for TDs.
    
    Use memory_region_init_ram_guest_memfd() to allocate the MemoryRegion
    for TDVF because it needs to be located at private memory.
    
    Also store the MemoryRegion pointer of TDVF since the shared ramblock of
    it can be discared after it gets copied to private ramblock.
    
    Signed-off-by: Chao Peng <chao.p.peng@linux.intel.com>
    Co-developed-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>

--- target/i386/kvm/tdx.c
+++ target/i386/kvm/tdx.c
@@ -117,6 +117,12 @@ static int get_tdx_capabilities(Error **errp)
     return 0;
 }
 
+void tdx_set_tdvf_region(MemoryRegion *tdvf_mr)
+{
+    assert(!tdx_guest->tdvf_mr);
+    tdx_guest->tdvf_mr = tdvf_mr;
+}
+
 static int tdx_kvm_type(X86ConfidentialGuest *cg)
 {
     /* Do the object check */
