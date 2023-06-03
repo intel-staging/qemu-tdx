3fdd8072da466d8d6c3da3840269bd7a3ef09293
Isaku Yamahata <isaku.yamahata@intel.com>
2023-06-02 18:36:56 -0700

    HostMem: Add private property and associate it with RAM_KVM_GMEM
    Add a new property "private" to memory backends. When it's set to true,
    it indicates the RAMblock of the backend also requires kvm gmem.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>

--- backends/hostmem-file.c
+++ backends/hostmem-file.c
@@ -56,6 +56,7 @@ file_backend_memory_alloc(HostMemoryBackend *backend, Error **errp)
     name = host_memory_backend_get_name(backend);
     ram_flags = backend->share ? RAM_SHARED : 0;
     ram_flags |= backend->reserve ? 0 : RAM_NORESERVE;
+    ram_flags |= backend->private ? RAM_KVM_GMEM : 0;
     ram_flags |= fb->is_pmem ? RAM_PMEM : 0;
     ram_flags |= RAM_NAMED_FILE;
     memory_region_init_ram_from_file(&backend->mr, OBJECT(backend), name,
