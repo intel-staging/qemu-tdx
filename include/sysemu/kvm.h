8f009c61ec65cb15431fef247d7c440003b2d961
Xiaoyao Li <xiaoyao.li@intel.com>
2023-07-31 04:14:28 -0400

    kvm: Introduce support for memory_attributes
    Introcude the helper functions to set the attributes of a range of
    memory to private and shared.
    
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>

--- include/sysemu/kvm.h
+++ include/sysemu/kvm.h
@@ -580,4 +580,7 @@ bool kvm_arch_cpu_check_are_resettable(void);
 bool kvm_dirty_ring_enabled(void);
 
 uint32_t kvm_dirty_ring_size(void);
+
+int kvm_set_memory_attributes_private(hwaddr start, hwaddr size);
+int kvm_set_memory_attributes_shared(hwaddr start, hwaddr size);
 #endif
