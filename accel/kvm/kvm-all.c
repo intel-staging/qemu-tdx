intel/qemu-td-partitioning

i386/tdx: handle TDG.VP.VMCALL<MapGPA> hypercall
MapGPA is a hypercall to convert GPA from/to private GPA to/from shared GPA.
As the conversion function is already implemented as kvm_convert_memory,
wire it to TDX hypercall exit.

Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

accel/kvm/kvm-all.c
@@ -2859,7 +2859,7 @@ int kvm_encrypt_reg_region(hwaddr start, hwaddr size, bool reg_region)
     return r;
 }
 
-static int kvm_convert_memory(hwaddr start, hwaddr size, bool shared_to_private)
+int kvm_convert_memory(hwaddr start, hwaddr size, bool shared_to_private)
 {
     MemoryRegionSection section;
     void *addr;

include/sysemu/kvm.h
@@ -585,4 +585,5 @@ bool kvm_dirty_ring_enabled(void);
 uint32_t kvm_dirty_ring_size(void);
 
 int kvm_encrypt_reg_region(hwaddr start, hwaddr size, bool reg_region);
+int kvm_convert_memory(hwaddr start, hwaddr size, bool shared_to_private);
 #endif

intel-staging/qemu-tdx

Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>

accel/kvm/kvm-all.c
@@ -3087,7 +3087,7 @@ static void kvm_eat_signals(CPUState *cpu)
     } while (sigismember(&chkset, SIG_IPI));
 }
 
-static int kvm_convert_memory(hwaddr start, hwaddr size, bool to_private)
+int kvm_convert_memory(hwaddr start, hwaddr size, bool to_private)
 {
     MemoryRegionSection section;
     ram_addr_t offset;

include/sysemu/kvm.h
@@ -576,4 +576,6 @@ int kvm_create_guest_memfd(uint64_t size, uint64_t flags, Error **errp);
 
 int kvm_set_memory_attributes_private(hwaddr start, hwaddr size);
 int kvm_set_memory_attributes_shared(hwaddr start, hwaddr size);
+
+int kvm_convert_memory(hwaddr start, hwaddr size, bool to_private);
 #endif