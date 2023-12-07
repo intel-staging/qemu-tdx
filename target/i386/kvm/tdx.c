ef1f24596accc5a2c8e8f6cbc44bf6c12848f82c
Xiaoyao Li <xiaoyao.li@intel.com>
2023-12-07 02:29:16 -0500

    i386/tdx: Add property sept-ve-disable for tdx-guest object
    Bit 28 of TD attribute, named SEPT_VE_DISABLE. When set to 1, it disables
    EPT violation conversion to #VE on guest TD access of PENDING pages.
    
    Some guest OS (e.g., Linux TD guest) may require this bit as 1.
    Otherwise refuse to boot.
    
    Add sept-ve-disable property for tdx-guest object, for user to configure
    this bit.
    
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Acked-by: Gerd Hoffmann <kraxel@redhat.com>
    Acked-by: Markus Armbruster <armbru@redhat.com>
    ---
    Changes in v4:
    - collect Acked-by from Markus
    
    Changes in v3:
    - update the comment of property @sept-ve-disable to make it more
      descriptive and use new format. (Daniel and Markus)

--- target/i386/kvm/tdx.c
+++ target/i386/kvm/tdx.c
@@ -20,6 +20,8 @@
 #include "kvm_i386.h"
 #include "tdx.h"
 
+#define TDX_TD_ATTRIBUTES_SEPT_VE_DISABLE   BIT_ULL(28)
+
 static TdxGuest *tdx_guest;
 
 static struct kvm_tdx_capabilities *tdx_caps;
@@ -173,6 +175,24 @@ int tdx_pre_create_vcpu(CPUState *cpu, Error **errp)
     return 0;
 }
 
+static bool tdx_guest_get_sept_ve_disable(Object *obj, Error **errp)
+{
+    TdxGuest *tdx = TDX_GUEST(obj);
+
+    return !!(tdx->attributes & TDX_TD_ATTRIBUTES_SEPT_VE_DISABLE);
+}
+
+static void tdx_guest_set_sept_ve_disable(Object *obj, bool value, Error **errp)
+{
+    TdxGuest *tdx = TDX_GUEST(obj);
+
+    if (value) {
+        tdx->attributes |= TDX_TD_ATTRIBUTES_SEPT_VE_DISABLE;
+    } else {
+        tdx->attributes &= ~TDX_TD_ATTRIBUTES_SEPT_VE_DISABLE;
+    }
+}
+
 /* tdx guest */
 OBJECT_DEFINE_TYPE_WITH_INTERFACES(TdxGuest,
                                    tdx_guest,
@@ -190,6 +210,10 @@ static void tdx_guest_init(Object *obj)
     tdx->attributes = 0;
 
     qemu_mutex_init(&tdx->lock);
+ 
+    object_property_add_bool(obj, "sept-ve-disable",
+                             tdx_guest_get_sept_ve_disable,
+                             tdx_guest_set_sept_ve_disable);
 }
 
 static void tdx_guest_finalize(Object *obj)
