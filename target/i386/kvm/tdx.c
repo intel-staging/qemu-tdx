68fef12a6ab085dcca88c767480775b5281e235c
Xiaoyao Li <xiaoyao.li@intel.com>
2023-12-07 02:27:28 -0500

    i386/tdx: Initialize TDX before creating TD vcpus
    Invoke KVM_TDX_INIT in kvm_arch_pre_create_vcpu() that KVM_TDX_INIT
    configures global TD configurations, e.g. the canonical CPUID config,
    and must be executed prior to creating vCPUs.
    
    Use kvm_x86_arch_cpuid() to setup the CPUID settings for TDX VM.
    
    Note, this doesn't address the fact that QEMU may change the CPUID
    configuration when creating vCPUs, i.e. punts on refactoring QEMU to
    provide a stable CPUID config prior to kvm_arch_init().
    
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Acked-by: Gerd Hoffmann <kraxel@redhat.com>
    Acked-by: Markus Armbruster <armbru@redhat.com>
    ---
    Changes in v4:
    - mark init_vm with g_autofree() and use QEMU_LOCK_GUARD() to eliminate
      the goto labels; (Daniel)
    Changes in v3:
    - Pass @errp in tdx_pre_create_vcpu() and pass error info to it. (Daniel)

--- target/i386/kvm/tdx.c
+++ target/i386/kvm/tdx.c
@@ -134,6 +134,45 @@ static int tdx_kvm_init(ConfidentialGuestSupport *cgs, Error **errp)
     return 0;
 }
 
+int tdx_pre_create_vcpu(CPUState *cpu, Error **errp)
+{
+    MachineState *ms = MACHINE(qdev_get_machine());
+    X86CPU *x86cpu = X86_CPU(cpu);
+    CPUX86State *env = &x86cpu->env;
+    g_autofree struct kvm_tdx_init_vm *init_vm = NULL;
+    int r = 0;
+
+    QEMU_LOCK_GUARD(&tdx_guest->lock);
+    if (tdx_guest->initialized) {
+        return r;
+    }
+
+    init_vm = g_malloc0(sizeof(struct kvm_tdx_init_vm) +
+                        sizeof(struct kvm_cpuid_entry2) * KVM_MAX_CPUID_ENTRIES);
+
+    r = kvm_vm_enable_cap(kvm_state, KVM_CAP_MAX_VCPUS, 0, ms->smp.cpus);
+    if (r < 0) {
+        error_setg(errp, "Unable to set MAX VCPUS to %d", ms->smp.cpus);
+        return r;
+    }
+
+    init_vm->cpuid.nent = kvm_x86_build_cpuid(env, init_vm->cpuid.entries, 0);
+
+    init_vm->attributes = tdx_guest->attributes;
+
+    do {
+        r = tdx_vm_ioctl(KVM_TDX_INIT_VM, 0, init_vm);
+    } while (r == -EAGAIN);
+    if (r < 0) {
+        error_setg_errno(errp, -r, "KVM_TDX_INIT_VM failed");
+        return r;
+    }
+
+    tdx_guest->initialized = true;
+
+    return 0;
+}
+
 /* tdx guest */
 OBJECT_DEFINE_TYPE_WITH_INTERFACES(TdxGuest,
                                    tdx_guest,
@@ -149,6 +188,8 @@ static void tdx_guest_init(Object *obj)
 
     cgs->require_guest_memfd = true;
     tdx->attributes = 0;
+
+    qemu_mutex_init(&tdx->lock);
 }
 
 static void tdx_guest_finalize(Object *obj)
