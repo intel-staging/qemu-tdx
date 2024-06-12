831bb27e876eccefdaac4ca4adc0927996c3eee7
Chenyi Qiang <chenyi.qiang@intel.com>
2024-06-12 09:22:16 +0000

    guest_memfd: Introduce an object to manage the guest-memfd with RamDiscardManager
    As the commit 852f0048f3 ("RAMBlock: make guest_memfd require
    uncoordinated discard") highlighted, some subsystems like VFIO might
    disable ram block discard. However, guest_memfd relies on the discard
    operation to perform page conversion between private and shared memory.
    This can lead to stale IOMMU mapping issue when assigning a hardware
    device to a confidential guest via shared memory (unprotected memory
    pages). Blocking shared page discard can solve this problem, but it
    could cause guests to consume twice the memory with VFIO, which is not
    acceptable in some cases. An alternative solution is to convey other
    systems like VFIO to refresh its outdated IOMMU mappings.
    
    RamDiscardManager is an existing concept (used by virtio-mem) to adjust
    VFIO mappings in relation to VM page assignement. Effectively page
    conversion is similar to hot-removing a page in one mode and adding it
    back in the other, so the similar work that needs to happen in response
    to virtio-mem changes needs to happen for page conversion events.
    Introduce the RamDiscardManager to guest_memfd to achieve it.
    
    However, Implementing the RamDiscardManager interface poses a challenge
    as guest_memfd is not an object, instead, it is contained within RamBlock
    and is indicated by a RAM_GUEST_MEMFD flag upon creation.
    
    One option is to implement the interface in HostMemoryBackend. Any
    guest_memfd-backed host memory backend can register itself in the target
    MemoryRegion. However, this solution doesn't cover the scenario where a
    guest_memfd MemoryRegion doesn't belong to the HostMemoryBackend, e.g.
    the virtual BIOS MemoryRegion.
    
    Thus, implement the second option, which involves defining an object type
    named guest_memfd_manager with the RamDiscardManager interface. Upon
    creation of guest_memfd, a new guest_memfd_manager object can be
    instantiated and registered to the managed guest_memfd MemoryRegion to
    handle the page conversion events.
    
    In the context of guest_memfd, the discarded state signifies that the
    page is private, while the populated state indicated that the page is
    shared. The state of the memory is tracked at the granularity of the
    host page size (i.e. block_size), as the minimum conversion size can be
    one page per request. In addition, VFIO expects the DMA mapping for a
    specific iova to be mapped and unmapped with the same granularity.
    However, there's no guarantee that the confidential guest won't
    partially convert the pages. For instance the confidential guest may
    flip a 2M page from private to shared and later flip the first 4K
    sub-range from shared to private. To prevent such invalid cases, all
    operations are performed with a 4K granularity.
    
    Signed-off-by: Chenyi Qiang <chenyi.qiang@intel.com>

---
+++ include/sysemu/guest-memfd-manager.h
@@ -0,0 +1,40 @@
+/*
+ * QEMU guest memfd manager
+ *
+ * Copyright Intel
+ *
+ * Author:
+ *      Chenyi Qiang <chenyi.qiang@intel.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory
+ *
+ */
+
+#ifndef SYSEMU_GUEST_MEMFD_MANAGER_H
+#define SYSEMU_GUEST_MEMFD_MANAGER_H
+
+#include "sysemu/hostmem.h"
+
+#define TYPE_GUEST_MEMFD_MANAGER "guest-memfd-manager"
+
+OBJECT_DECLARE_SIMPLE_TYPE(GuestMemfdManager, GUEST_MEMFD_MANAGER)
+
+struct GuestMemfdManager {
+    Object parent;
+
+    /* Managed memory region. */
+    MemoryRegion *mr;
+
+    /* bitmap used to track discard/private memory */
+    int32_t discard_bitmap_size;
+    unsigned long *discard_bitmap;
+
+    /* block size and alignment */
+    uint64_t block_size;
+
+    /* listeners to notify on plug/unplug activity. */
+    QLIST_HEAD(, RamDiscardListener) rdl_list;
+};
+
+#endif
