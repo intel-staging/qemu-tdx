4f35666b770da33a22d7b75a7559564da2fcad45
Chenyi Qiang <chenyi.qiang@intel.com>
2024-06-21 03:24:55 +0000

    RAMBlock: Make guest_memfd require coordinate discard
    As guest_memfd is now managed by memory_attribute_manager with
    RamDiscardManager, only block uncoordinated discard.
    
    Signed-off-by: Chenyi Qiang <chenyi.qiang@intel.com>
    ---
    Changes in v2:
        - Change the ram_block_discard_require(false) to
          ram_block_coordinated_discard_require(false).

--- system/physmem.c
+++ system/physmem.c
@@ -1872,7 +1872,7 @@ static void ram_block_add(RAMBlock *new_block, Error **errp)
         assert(kvm_enabled());
         assert(new_block->guest_memfd < 0);
 
-        ret = ram_block_discard_require(true);
+        ret = ram_block_coordinated_discard_require(true);
         if (ret < 0) {
             error_setg_errno(errp, -ret,
                              "cannot set up private guest memory: discard currently blocked");
@@ -1892,7 +1892,7 @@ static void ram_block_add(RAMBlock *new_block, Error **errp)
             error_setg(errp, "Failed to realize memory attribute manager");
             object_unref(OBJECT(new_block->memory_attribute_manager));
             close(new_block->guest_memfd);
-            ram_block_discard_require(false);
+            ram_block_coordinated_discard_require(false);
             qemu_mutex_unlock_ramlist();
             goto out_free;
         }
@@ -2152,7 +2152,7 @@ static void reclaim_ramblock(RAMBlock *block)
         memory_attribute_manager_unrealize(block->memory_attribute_manager);
         object_unref(OBJECT(block->memory_attribute_manager));
         close(block->guest_memfd);
-        ram_block_discard_require(false);
+        ram_block_coordinated_discard_require(false);
     }
 
     g_free(block);
