e7769adabb6dc2661186d56f192c747f1c943b58
Xiaoyao Li <xiaoyao.li@intel.com>
2024-07-10 08:38:39 -0400

    i386/tdx: Implement mask_cpuid_features() for TDX
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>

--- target/i386/kvm/tdx.c
+++ target/i386/kvm/tdx.c
@@ -34,6 +34,8 @@
 #include "tdx-quote-generator.h"
 #include "../cpu-internal.h"
 
+#include "standard-headers/asm-x86/kvm_para.h"
+
 #define TDX_MIN_TSC_FREQUENCY_KHZ   (100 * 1000)
 #define TDX_MAX_TSC_FREQUENCY_KHZ   (10 * 1000 * 1000)
 
@@ -41,6 +43,14 @@
 #define TDX_TD_ATTRIBUTES_SEPT_VE_DISABLE   BIT_ULL(28)
 #define TDX_TD_ATTRIBUTES_PKS               BIT_ULL(30)
 #define TDX_TD_ATTRIBUTES_PERFMON           BIT_ULL(63)
+
+#define TDX_SUPPORTED_KVM_FEATURES  ((1U << KVM_FEATURE_NOP_IO_DELAY) | \
+                                     (1U << KVM_FEATURE_PV_UNHALT) | \
+                                     (1U << KVM_FEATURE_PV_TLB_FLUSH) | \
+                                     (1U << KVM_FEATURE_PV_SEND_IPI) | \
+                                     (1U << KVM_FEATURE_POLL_CONTROL) | \
+                                     (1U << KVM_FEATURE_PV_SCHED_YIELD) | \
+                                     (1U << KVM_FEATURE_MSI_EXT_DEST_ID))
 
 static TdxGuest *tdx_guest;
 
@@ -419,6 +429,54 @@ static int tdx_kvm_init(ConfidentialGuestSupport *cgs, Error **errp)
 
     tdx_guest = tdx;
     return 0;
+}
+
+static uint32_t tdx_mask_cpuid_features(X86ConfidentialGuest *cg,
+                                        uint32_t feature, uint32_t index,
+                                        int reg, uint32_t value)
+{
+    switch(feature) {
+        case 0x1:
+            if (reg == R_ECX) {
+                value &= ~(CPUID_EXT_VMX | CPUID_EXT_SMX);
+                value |= (CPUID_EXT_DSCPL | CPUID_EXT_PDCM);
+            } else if (reg == R_EDX) {
+                value &= ~CPUID_PSE36;
+            }
+            break;
+        case 0x7:
+            if (reg == R_EBX) {
+                value &= ~(CPUID_7_0_EBX_TSC_ADJUST | CPUID_7_0_EBX_SGX);
+                // QEMU Intel PT support is broken
+                value &= ~CPUID_7_0_EBX_INTEL_PT;
+                value |= CPUID_7_0_EBX_FDP_EXCPTION_ONLY |
+                         CPUID_7_0_EBX_FCS_FDS_DEPRE;
+            } else if (reg == R_ECX) {
+                value &= ~CPUID_7_0_ECX_SGX_LC;
+            } else if (reg == R_EDX) {
+                value |= CPUID_7_0_EDX_CORE_CAPABILITY;
+            }
+            break;
+        case 0x40000001:
+            if (reg == R_EAX) {
+                value &= TDX_SUPPORTED_KVM_FEATURES;
+            }
+            break;
+        case 0x80000007:
+            if (reg == R_EDX) {
+                value |= CPUID_APM_INVTSC;
+            }
+            break;
+        case 0x80000008:
+            if (reg == R_EBX) {
+                value &= CPUID_8000_0008_EBX_WBNOINVD;
+            }
+            break;
+        default:
+            return value;
+    }
+
+    return value;
 }
 
 static int tdx_validate_attributes(TdxGuest *tdx, Error **errp)
@@ -1117,4 +1175,5 @@ static void tdx_guest_class_init(ObjectClass *oc, void *data)
 
     klass->kvm_init = tdx_kvm_init;
     x86_klass->kvm_type = tdx_kvm_type;
+    x86_klass->mask_cpuid_features = tdx_mask_cpuid_features;
 }
