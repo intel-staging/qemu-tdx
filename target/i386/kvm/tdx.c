bc2ac53c9547a0f24739ec3073d937c9faec44af
Xiaoyao Li <xiaoyao.li@intel.com>
2024-07-11 06:37:44 -0400

    i386/tdx: Implement ->adjust_cpuid()
    TDX module always allows reading of MSR_IA32_PERF_CAPABILITIES. Thus
    always advertise CPUID_EXT_PDCM for TD guest.
    
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>

--- target/i386/kvm/tdx.c
+++ target/i386/kvm/tdx.c
@@ -466,6 +466,17 @@ static uint32_t tdx_mask_cpuid_features(X86ConfidentialGuest *cg,
     return value;
 }
 
+static void tdx_adjust_cpuid(X86ConfidentialGuest *cg, uint32_t index, uint32_t count,
+                            uint32_t *eax, uint32_t *ebx, uint32_t *ecx, uint32_t *edx)
+{
+    switch (index) {
+        case 0x1:
+            /* TDX always advertise IA32_PERF_CAPABILITIES */
+            *ecx |= CPUID_EXT_PDCM;
+            break;
+    }
+}
+
 static int tdx_validate_attributes(TdxGuest *tdx, Error **errp)
 {
     if ((tdx->attributes & ~tdx_caps->supported_attrs)) {
@@ -1164,4 +1175,5 @@ static void tdx_guest_class_init(ObjectClass *oc, void *data)
     x86_klass->kvm_type = tdx_kvm_type;
     x86_klass->cpu_post_init = tdx_cpu_post_init;
     x86_klass->mask_cpuid_features = tdx_mask_cpuid_features;
+    x86_klass->adjust_cpuid = tdx_adjust_cpuid;
 }
