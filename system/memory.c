d572546005e7d26607ef07293cce033c320f3463
Chenyi Qiang <chenyi.qiang@intel.com>
2024-12-09 17:31:01 +0800

    memory: Export a helper to get intersection of a MemoryRegionSection with a given range
    Rename the helper to memory_region_section_intersect_range() to make it
    more generic. Meanwhile, define the @end as Int128 and replace the
    related operations with Int128_* format since the helper is exported as
    a wider API.
    
    Suggested-by: Alexey Kardashevskiy <aik@amd.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Signed-off-by: Chenyi Qiang <chenyi.qiang@intel.com>
    ---
    Changes in v2:
    - Add Reviewed-by from David
    - Define the @end as Int128 and use the related Int128_* ops as a wilder
      API (Alexey)

--- system/memory.c
+++ system/memory.c
@@ -2898,6 +2898,23 @@ void memory_region_section_free_copy(MemoryRegionSection *s)
     g_free(s);
 }
 
+bool memory_region_section_intersect_range(MemoryRegionSection *s,
+                                           uint64_t offset, uint64_t size)
+{
+    uint64_t start = MAX(s->offset_within_region, offset);
+    Int128 end = int128_min(int128_add(int128_make64(s->offset_within_region), s->size),
+                            int128_add(int128_make64(offset), int128_make64(size)));
+
+    if (int128_le(end, int128_make64(start))) {
+        return false;
+    }
+
+    s->offset_within_address_space += start - s->offset_within_region;
+    s->offset_within_region = start;
+    s->size = int128_sub(end, int128_make64(start));
+    return true;
+}
+
 bool memory_region_present(MemoryRegion *container, hwaddr addr)
 {
     MemoryRegion *mr;
