923ba13697679303b666332c8e674318b00a861c
Chenyi Qiang <chenyi.qiang@intel.com>
2025-02-11 11:39:01 +0800

    memory: Change memory_region_set_ram_discard_manager() to return the result
    Modify memory_region_set_ram_discard_manager() to return false if a
    RamDiscardManager is already set in the MemoryRegion. The caller must
    handle this failure, such as having virtio-mem undo its actions and fail
    the realize() process. Opportunistically move the call earlier to avoid
    complex error handling.
    
    This change is beneficial when introducing a new RamDiscardManager
    instance besides virtio-mem. After
    ram_block_coordinated_discard_require(true) unlocks all
    RamDiscardManager instances, only one instance is allowed to be set for
    a MemoryRegion at present.
    
    Suggested-by: David Hildenbrand <david@redhat.com>
    Signed-off-by: Chenyi Qiang <chenyi.qiang@intel.com>
    ---
    Changes in v3:
        - Move set_ram_discard_manager() up to avoid a g_free()
        - Clean up set_ram_discard_manager() definition
    
    Changes in v2:
        - newly added.

--- hw/virtio/virtio-mem.c
+++ hw/virtio/virtio-mem.c
@@ -1048,6 +1048,17 @@ static void virtio_mem_device_realize(DeviceState *dev, Error **errp)
     }
 
     /*
+     * Set ourselves as RamDiscardManager before the plug handler maps the
+     * memory region and exposes it via an address space.
+     */
+    if (memory_region_set_ram_discard_manager(&vmem->memdev->mr,
+                                              RAM_DISCARD_MANAGER(vmem))) {
+        error_setg(errp, "Failed to set RamDiscardManager");
+        ram_block_coordinated_discard_require(false);
+        return;
+    }
+
+    /*
      * We don't know at this point whether shared RAM is migrated using
      * QEMU or migrated using the file content. "x-ignore-shared" will be
      * configured after realizing the device. So in case we have an
@@ -1122,25 +1133,12 @@ static void virtio_mem_device_realize(DeviceState *dev, Error **errp)
     vmem->system_reset = VIRTIO_MEM_SYSTEM_RESET(obj);
     vmem->system_reset->vmem = vmem;
     qemu_register_resettable(obj);
-
-    /*
-     * Set ourselves as RamDiscardManager before the plug handler maps the
-     * memory region and exposes it via an address space.
-     */
-    memory_region_set_ram_discard_manager(&vmem->memdev->mr,
-                                          RAM_DISCARD_MANAGER(vmem));
 }
 
 static void virtio_mem_device_unrealize(DeviceState *dev)
 {
     VirtIODevice *vdev = VIRTIO_DEVICE(dev);
     VirtIOMEM *vmem = VIRTIO_MEM(dev);
-
-    /*
-     * The unplug handler unmapped the memory region, it cannot be
-     * found via an address space anymore. Unset ourselves.
-     */
-    memory_region_set_ram_discard_manager(&vmem->memdev->mr, NULL);
 
     qemu_unregister_resettable(OBJECT(vmem->system_reset));
     object_unref(OBJECT(vmem->system_reset));
@@ -1154,6 +1152,11 @@ static void virtio_mem_device_unrealize(DeviceState *dev)
     virtio_del_queue(vdev, 0);
     virtio_cleanup(vdev);
     g_free(vmem->bitmap);
+    /*
+     * The unplug handler unmapped the memory region, it cannot be
+     * found via an address space anymore. Unset ourselves.
+     */
+    memory_region_set_ram_discard_manager(&vmem->memdev->mr, NULL);
     ram_block_coordinated_discard_require(false);
 }
 
