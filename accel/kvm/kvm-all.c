493dc12bc776837cd502a026584e71a94d231917
Xiaoyao Li <xiaoyao.li@intel.com>
2025-07-18 15:38:47 +0800

    kvm/guest_memfd: Allow create guest_memfd with mmap support
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>

--- accel/kvm/kvm-all.c
+++ accel/kvm/kvm-all.c
@@ -106,6 +106,7 @@ static int kvm_sstep_flags;
 static bool kvm_immediate_exit;
 static uint64_t kvm_supported_memory_attributes;
 static bool kvm_guest_memfd_supported;
+bool kvm_guest_memfd_mmap_supported;
 static hwaddr kvm_max_slot_size = ~0;
 
 static const KVMCapabilityInfo kvm_required_capabilites[] = {
@@ -2783,6 +2784,8 @@ static int kvm_init(AccelState *as, MachineState *ms)
     kvm_guest_memfd_supported =
         kvm_vm_check_extension(s, KVM_CAP_GUEST_MEMFD) &&
         kvm_vm_check_extension(s, KVM_CAP_USER_MEMORY2);
+    kvm_guest_memfd_mmap_supported =
+        kvm_vm_check_extension(s, KVM_CAP_GMEM_MMAP);
     kvm_pre_fault_memory_supported = kvm_vm_check_extension(s, KVM_CAP_PRE_FAULT_MEMORY);
 
     if (s->kernel_irqchip_split == ON_OFF_AUTO_AUTO) {
@@ -4497,6 +4500,11 @@ int kvm_create_guest_memfd(uint64_t size, uint64_t flags, Error **errp)
 
     if (!kvm_guest_memfd_supported) {
         error_setg(errp, "KVM does not support guest_memfd");
+        return -1;
+    }
+
+    if (flags & GUEST_MEMFD_FLAG_MMAP && !kvm_guest_memfd_mmap_supported) {
+        error_setg(errp, "KVM does not support guest_memfd with mmap");
         return -1;
     }
 
