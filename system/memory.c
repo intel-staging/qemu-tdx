944aa823873140fba38d5d5bacf299fa7ab9e463
Xiaoyao Li <xiaoyao.li@intel.com>
2025-07-23 01:02:56 +0800

    memory: Introduce memory_region_has_private_memory()
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>

--- system/memory.c
+++ system/memory.c
@@ -1880,6 +1880,11 @@ bool memory_region_has_guest_memfd(MemoryRegion *mr)
     return mr->ram_block && mr->ram_block->guest_memfd >= 0;
 }
 
+bool memory_region_has_private_memory(MemoryRegion *mr)
+{
+    return mr->ram_block && mr->ram_block->flags & RAM_PRIVATE_MEMORY;
+}
+
 uint8_t memory_region_get_dirty_log_mask(MemoryRegion *mr)
 {
     uint8_t mask = mr->dirty_log_mask;
